8134893a43650313f7fb7b4cd2814df1ef5db433
atif-phantom <atif@phantom.us>
2015-11-30 11:11:06 -0800

    Modified VT app playbook to:
    * Use dictionary lists as parameters instead of the comma separated format (since this type of input is being phased out)
    * Added sleep in between phantom.act(...) calls to try and _not_ hit the VT rate limit.

--- virustotal_app.py
+++ virustotal_app.py
@@ -4,6 +4,7 @@ This playbook runs all the VirusTotal actions one by one.
 
 import phantom.rules as phantom
 import json
+import time
 
 def ip_reputation_cb(action, success, incident, results, handle):
 
@@ -17,7 +18,9 @@ def url_reputation_cb(action, success, incident, results, handle):
     if not success:
         return
 
-    phantom.act('ip reputation', parameters=[{ "ip" : "134.170.188.221, 10.10.0.206" }], assets=["virustotal_private"], callback=ip_reputation_cb)
+    # Add sleep here to _not_ hit the VT rate limit.
+    time.sleep(1)
+    phantom.act('ip reputation', parameters=[{ "ip" : "134.170.188.221"}], assets=["virustotal_private"], callback=ip_reputation_cb)
 
     return
 
@@ -26,7 +29,9 @@ def domain_reputation_cb(action, success, incident, results, handle):
     if not success:
         return
 
-    phantom.act('url reputation', parameters=[{ "url" : "www.advancesrl.eu/tjjyeqyfjz/gmiuxfhgsb.html, www.howaboutthatrightnownotpresent.com" }], assets=["virustotal_private"], callback=url_reputation_cb)
+    # Add sleep here to _not_ hit the VT rate limit.
+    time.sleep(1)
+    phantom.act('url reputation', parameters=[{ "url" : "http://www.advancesrl.eu/tjjyeqyfjz/gmiuxfhgsb.html"}], assets=["virustotal_private"], callback=url_reputation_cb)
 
     return
 
@@ -35,14 +40,16 @@ def file_reputation_cb(action, success, incident, results, handle):
     if not success:
         return
 
-    phantom.act('domain reputation', parameters=[{ "domain" : "amazon.com, google.com" }], assets=["virustotal_private"], callback=domain_reputation_cb)
+    # Add sleep here to _not_ hit the VT rate limit. Not needed if VT Private API is used.
+    time.sleep(1)
+    phantom.act('domain reputation', parameters=[{ "domain" : "amazon.com"}], assets=["virustotal_private"], callback=domain_reputation_cb)
 
     return
 
 
 def on_start(incident):
 
-    phantom.act('file reputation', parameters=[{ "hash" : "7896b9b34bdbedbe7bdc6d446ecb09d5, 99017f6eebbac24f351415dd410d522d, ce0296e2d77ec3bb112e270fc260f274, 3828f4f998aefe0282e4c9cd642ac110dd3745d6" }], assets=["virustotal_private"], callback=file_reputation_cb)
+    phantom.act('file reputation', parameters=[{ "hash" : "7896b9b34bdbedbe7bdc6d446ecb09d5"}, {"hash": "99017f6eebbac24f351415dd410d522d"}], assets=["virustotal_private"], callback=file_reputation_cb)
 
     return
 
