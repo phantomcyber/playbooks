222b4e7a6296a2bacf57c751e953bc0c07bb3d8f
atif-phantom <atif@phantom.us>
2015-12-22 14:07:51 -0800

    Test Connectivity Playbook

---
+++ test_connectivity.py
@@ -0,0 +1,23 @@
+"""This playbook runs the 'test connectivity' action on all possible assets currently configured"""
+
+import phantom.rules as phantom
+import json
+
+def get_report_cb(action, success, incident, results, handle):
+
+    if not success:
+        return
+
+    return
+
+def on_start(incident):
+
+    phantom.act('test connectivity', parameters=[], callback=get_report_cb)
+
+    return
+
+def on_finish(incident, summary):
+
+    phantom.debug("Summary: " + summary)
+
+    return
