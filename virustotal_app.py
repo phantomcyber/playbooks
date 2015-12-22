690c248e69af12b3b50fb6739dbba59adbf8dd69
atif-phantom <atif@phantom.us>
2015-12-21 19:35:33 -0800

    VT Playbook
    Modified the params passed to dictionary list, instead of the comma
    separated params

--- virustotal_app.py
+++ virustotal_app.py
@@ -17,7 +17,7 @@ def url_reputation_cb(action, success, incident, results, handle):
     if not success:
         return
 
-    phantom.act('ip reputation', parameters=[{ "ip" : "134.170.188.221, 10.10.0.206" }], assets=["virustotal_private"], callback=ip_reputation_cb)
+    phantom.act('ip reputation', parameters=[{ "ip" : "134.170.188.221"}, {"ip": "10.10.0.206" }], assets=["virustotal_private"], callback=ip_reputation_cb)
 
     return
 
@@ -26,7 +26,7 @@ def domain_reputation_cb(action, success, incident, results, handle):
     if not success:
         return
 
-    phantom.act('url reputation', parameters=[{ "url" : "www.advancesrl.eu/tjjyeqyfjz/gmiuxfhgsb.html, www.howaboutthatrightnownotpresent.com" }], assets=["virustotal_private"], callback=url_reputation_cb)
+    phantom.act('url reputation', parameters=[{ "url" : "www.advancesrl.eu/tjjyeqyfjz/gmiuxfhgsb.html"}, {"url": "www.howaboutthatrightnownotpresent.com" }], assets=["virustotal_private"], callback=url_reputation_cb)
 
     return
 
@@ -35,14 +35,16 @@ def file_reputation_cb(action, success, incident, results, handle):
     if not success:
         return
 
-    phantom.act('domain reputation', parameters=[{ "domain" : "amazon.com, google.com" }], assets=["virustotal_private"], callback=domain_reputation_cb)
+    phantom.act('domain reputation', parameters=[{ "domain" : "amazon.com"}, {"domain": "google.com" }], assets=["virustotal_private"], callback=domain_reputation_cb)
 
     return
 
 
 def on_start(incident):
 
-    phantom.act('file reputation', parameters=[{ "hash" : "7896b9b34bdbedbe7bdc6d446ecb09d5, 99017f6eebbac24f351415dd410d522d, ce0296e2d77ec3bb112e270fc260f274, 3828f4f998aefe0282e4c9cd642ac110dd3745d6" }], assets=["virustotal_private"], callback=file_reputation_cb)
+    phantom.act('file reputation', parameters=[{ "hash" : "7896b9b34bdbedbe7bdc6d446ecb09d5"}, 
+        {"hash": "99017f6eebbac24f351415dd410d522d"}, {"hash": "ce0296e2d77ec3bb112e270fc260f274"}, 
+        {"hash": "3828f4f998aefe0282e4c9cd642ac110dd3745d6" }], assets=["virustotal_private"], callback=file_reputation_cb)
 
     return
 
