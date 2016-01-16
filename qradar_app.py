41ec6f57e1c20b97d2e246cabd4fb05f63e8b96d
atif-phantom <atif@phantom.us>
2016-01-16 13:05:56 -0800

    Query valid offense ids
    Modified the playbook to only query a single offense id that is
    extracted from the results of the ‘list offenses’ action, instead of
    hard coded offense ids.

--- qradar_app.py
+++ qradar_app.py
@@ -8,6 +8,35 @@ def list_offenses_cb(action, success, incident, results, handle):
 
     if not success:
         return
+    
+    # dump the results
+    phantom.debug(json.dumps(results, indent=4))
+    
+    data = None
+    
+    # Get the data from results, ideally we would loop over the results
+    try:
+        data = results[0]['action_results'][0]['data']
+    except:
+        return
+        
+    if (not data):
+        return
+    
+    # data is a list of offenses, only work on the 1st one for now
+    try:
+        offense_id = data[0]['id']
+    except:
+        return
+    
+    # Execute all the actions that can be made on an offense id
+    phantom.act('get events', parameters=[{ "count" : "10",  "offense_id" : offense_id }], assets=["qradar_entr"], callback=get_events_cb)
+
+    phantom.act('get flows', parameters=[{ "count" : "10",  "ip" : "10.16.1.60",  "offense_id" : offense_id }], assets=["qradar_entr"], callback=get_flows_cb)
+
+    phantom.act('get flows', parameters=[{ "count" : "10",  "offense_id" : offense_id }], assets=["qradar_entr"], callback=get_flows1_cb)
+
+    phantom.act('offense details', parameters=[{ "offense_id" : offense_id }], assets=["qradar_entr"], callback=offense_details_cb)
 
     return
 
@@ -42,15 +71,8 @@ def offense_details_cb(action, success, incident, results, handle):
 
 def on_start(incident):
 
+    # First get the list of offenses
     phantom.act('list offenses', parameters=[{ "count" : "10" }], assets=["qradar_entr"], callback=list_offenses_cb)
-
-    phantom.act('get events', parameters=[{ "count" : "10",  "offense_id" : "9" }], assets=["qradar_entr"], callback=get_events_cb)
-
-    phantom.act('get flows', parameters=[{ "count" : "10",  "ip" : "10.16.1.60",  "offense_id" : "1" }], assets=["qradar_entr"], callback=get_flows_cb)
-
-    phantom.act('get flows', parameters=[{ "count" : "10",  "offense_id" : "2" }], assets=["qradar_entr"], callback=get_flows1_cb)
-
-    phantom.act('offense details', parameters=[{ "offense_id" : "3" }], assets=["qradar_entr"], callback=offense_details_cb)
 
     return
 
