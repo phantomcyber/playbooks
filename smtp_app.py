c01c97f57598e9f49b4d24e6da6339bc2dbda0b5
atif-phantom <atif@phantom.us>
2016-02-24 15:44:01 -0800

    Added code to send html formatted email

--- smtp_app.py
+++ smtp_app.py
@@ -15,7 +15,15 @@ def send_email_cb(action, success, incident, results, handle):
 
 def on_start(incident):
 
-    phantom.act('send email', parameters=[{ "body" : "This is a test mail, Executed from a playbook",  "to" : "notifications@phanom.us",  "subject" : "Test Email from playbook" }], assets=["smtp"], callback=send_email_cb)
+    phantom.act('send email', parameters=[{ "body" : "This is a test mail, Executed from a playbook",  "to" : "notifications@phantom.us",  "subject" : "Test Email from playbook" }], assets=["smtp"], callback=send_email_cb)
+    html_body = '<html>'
+    html_body += 'This is a test mail,<br>'
+    html_body += 'Executed from a playbook for '
+    html_body += '<a href="{base_url}/container/{container_id}"><b>this container</b></a>.<br>'.format(base_url=phantom.get_base_url(), container_id=incident['id'])
+    html_body += '</html>'
+    
+    phantom.act('send email', parameters=[{ "body" : html_body,  "to" : "notifications@phantom.us",  "subject" : "Test HTML Email from playbook" }], assets=["smtp"], callback=send_email_cb)
+
 
     return
 
