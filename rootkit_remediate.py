5ff04fcbe260b02c739812231136696f9c252e6b
sourabh <sourabh@phantom.us>
2016-09-22 17:29:17 -0700

    rootkit_remediate playbook with visual representatio

--- rootkit_remediate.py
+++ rootkit_remediate.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 ##############################
 # Start - Global Code Block
 
-"""This playbook executes remediation actions to clear a rootkit infection on an endpoint """
+""" This playbook executes remediation actions to clear a rootkit infection on an endpoint """
 
 # End - Global Code block
 ##############################
@@ -15,6 +15,31 @@ def on_start(container):
     # call 'quarantine_device_1' block
     quarantine_device_1(container=container)
 
+    return
+
+def get_system_info_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'get_system_info_1' call
+    inputs_data_1 = phantom.collect2(container=container, datapath=['quarantine_device_1:artifact:*.cef.sourceAddress', 'quarantine_device_1:artifact:*.id'], action_results=results)
+
+    parameters = []
+    
+    # build parameters list for 'get_system_info_1' call
+    for inputs_item_1 in inputs_data_1:
+        if inputs_item_1[0]:
+            parameters.append({
+                'ip_hostname': inputs_item_1[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': inputs_item_1[1]},
+            })
+
+    if parameters:
+        phantom.act("get system info", parameters=parameters, assets=['vmwarevsphere'], callback=decision_1, name="get_system_info_1", parent_action=action)    
+    else:
+        phantom.error("'get_system_info_1' will not be executed due to lack of parameters")
+    
     return
 
 def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
@@ -46,53 +71,153 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     return
 
-def Send_Email_reverted(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'Send_Email_reverted' call
-
-    parameters = []
-    
-    # build parameters list for 'Send_Email_reverted' call
-    parameters.append({
-        'body': "A rootkit was discovered on a device, which was then reverted to an earlier VM snapshot.  See Phantom for more details.",
-        'to': "test@phantom.us",
-        'from': "admin@phantom.us",
-        'attachments': "",
-        'subject': "Rootkit was remediated",
-    })
-
-    if parameters:
-        phantom.act("send email", parameters=parameters, assets=['smtp'], name="Send_Email_reverted", parent_action=action)    
-    else:
-        phantom.error("'Send_Email_reverted' will not be executed due to lack of parameters")
-    
-    return
-
 def revert_vm_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
     
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     
     # collect data for 'revert_vm_1' call
-    filter_results_data_1 = phantom.collect2(container=container, datapath=["get_system_info_1:filtered-action_result.data.*.vmx_path", "get_system_info_1:filtered-action_result.parameter.context.artifact_id"], action_results=filtered_results)
+    filtered_results_data_1 = phantom.collect2(container=container, datapath=["get_system_info_1:filtered-action_result.data.*.vmx_path", "get_system_info_1:filtered-action_result.parameter.context.artifact_id"], action_results=filtered_results)
 
     parameters = []
     
     # build parameters list for 'revert_vm_1' call
-    for filter_results_item_1 in filter_results_data_1:
-        if filter_results_item_1[0]:
+    for filtered_results_item_1 in filtered_results_data_1:
+        if filtered_results_item_1[0]:
             parameters.append({
                 'snapshot': "",
-                'vmx_path': filter_results_item_1[0],
+                'vmx_path': filtered_results_item_1[0],
                 # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': filter_results_item_1[1]},
+                'context': {'artifact_id': filtered_results_item_1[1]},
             })
 
     if parameters:
         phantom.act("revert vm", parameters=parameters, assets=['vmwarevsphere'], callback=unquarantine_device_1, name="revert_vm_1")    
     else:
         phantom.error("'revert_vm_1' will not be executed due to lack of parameters")
+    
+    return
+
+def send_email_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'send_email_2' call
+
+    parameters = []
+    
+    # build parameters list for 'send_email_2' call
+    parameters.append({
+        'body': "",
+        'to': "",
+        'from': "",
+        'attachments': "",
+        'subject': "",
+    })
+
+    if parameters:
+        phantom.act("send email", parameters=parameters, assets=['smtp'], name="send_email_2", parent_action=action)    
+    else:
+        phantom.error("'send_email_2' will not be executed due to lack of parameters")
+    
+    return
+
+def unquarantine_device_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'unquarantine_device_1' call
+    inputs_data_1 = phantom.collect2(container=container, datapath=['revert_vm_1:artifact:*.cef.sourceAddress', 'revert_vm_1:artifact:*.id'], action_results=results)
+
+    parameters = []
+    
+    # build parameters list for 'unquarantine_device_1' call
+    for inputs_item_1 in inputs_data_1:
+        if inputs_item_1[0]:
+            parameters.append({
+                'ip_hostname': inputs_item_1[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': inputs_item_1[1]},
+            })
+
+    if parameters:
+        phantom.act("unquarantine device", parameters=parameters, assets=['carbonblack'], callback=Send_Email_reverted, name="unquarantine_device_1", parent_action=action)    
+    else:
+        phantom.error("'unquarantine_device_1' will not be executed due to lack of parameters")
+    
+    return
+
+def quarantine_device_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+
+    # collect data for 'quarantine_device_1' call
+    container_data = phantom.get_data("rkitdata", clear_data=True)
+
+    parameters = []
+    
+    # build parameters list for 'quarantine_device_1' call
+    for container_item in container_data:
+        if container_item[0]:
+            parameters.append({
+                'ip_hostname': container_item[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': container_item[1]},
+            })
+
+    if parameters:
+        phantom.act("quarantine device", parameters=parameters, assets=['carbonblack'], callback=get_system_info_1, name="quarantine_device_1")    
+    else:
+        phantom.error("'quarantine_device_1' will not be executed due to lack of parameters")
+    
+    return
+
+def get_system_info_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'get_system_info_2' call
+    filtered_results_data_1 = phantom.collect2(container=container, datapath=["get_system_info_1:filtered-action_result.data.*.ip", "get_system_info_1:filtered-action_result.parameter.context.artifact_id"], action_results=filtered_results)
+
+    parameters = []
+    
+    # build parameters list for 'get_system_info_2' call
+    for filtered_results_item_1 in filtered_results_data_1:
+        if filtered_results_item_1[0]:
+            parameters.append({
+                'ip_hostname': filtered_results_item_1[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': filtered_results_item_1[1]},
+            })
+
+    if parameters:
+        phantom.act("get system info", parameters=parameters, assets=['domainctrl1'], callback=disable_user_1, name="get_system_info_2")    
+    else:
+        phantom.error("'get_system_info_2' will not be executed due to lack of parameters")
+    
+    return
+
+def terminate_process_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'terminate_process_1' call
+    filtered_results_data_1 = phantom.collect2(container=container, datapath=["get_system_info_1:filtered-action_result.data.*.vm_hostname", "get_system_info_1:filtered-action_result.parameter.context.artifact_id"], action_results=filtered_results)
+
+    parameters = []
+    
+    # build parameters list for 'terminate_process_1' call
+    for filtered_results_item_1 in filtered_results_data_1:
+        if filtered_results_item_1[0]:
+            parameters.append({
+                'sensor_id': "",
+                'pid': "",
+                'ip_hostname': filtered_results_item_1[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': filtered_results_item_1[1]},
+            })
+
+    if parameters:
+        phantom.act("terminate process", parameters=parameters, assets=['carbonblack'], callback=join_create_ticket_1, name="terminate_process_1")    
+    else:
+        phantom.error("'terminate_process_1' will not be executed due to lack of parameters")
     
     return
 
@@ -133,30 +258,6 @@ def join_create_ticket_1(action=None, success=None, container=None, results=None
     
     return
 
-def send_email_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'send_email_2' call
-
-    parameters = []
-    
-    # build parameters list for 'send_email_2' call
-    parameters.append({
-        'body': "",
-        'to': "",
-        'from': "",
-        'attachments': "",
-        'subject': "",
-    })
-
-    if parameters:
-        phantom.act("send email", parameters=parameters, assets=['smtp'], name="send_email_2", parent_action=action)    
-    else:
-        phantom.error("'send_email_2' will not be executed due to lack of parameters")
-    
-    return
-
 def disable_user_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
     
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
@@ -182,128 +283,27 @@ def disable_user_1(action=None, success=None, container=None, results=None, hand
     
     return
 
-def terminate_process_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def Send_Email_reverted(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
     
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     
-    # collect data for 'terminate_process_1' call
-    filter_results_data_1 = phantom.collect2(container=container, datapath=["get_system_info_1:filtered-action_result.data.*.vm_hostname", "get_system_info_1:filtered-action_result.parameter.context.artifact_id"], action_results=filtered_results)
+    # collect data for 'Send_Email_reverted' call
 
     parameters = []
     
-    # build parameters list for 'terminate_process_1' call
-    for filter_results_item_1 in filter_results_data_1:
-        if filter_results_item_1[0]:
-            parameters.append({
-                'ip_hostname': filter_results_item_1[0],
-                'sensor_id': "",
-                'pid': "",
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': filter_results_item_1[1]},
-            })
+    # build parameters list for 'Send_Email_reverted' call
+    parameters.append({
+        'body': "A rootkit was discovered on a device, which was then reverted to an earlier VM snapshot.  See Phantom for more details.",
+        'to': "test@phantom.us",
+        'from': "admin@phantom.us",
+        'attachments': "",
+        'subject': "Rootkit was remediated",
+    })
 
     if parameters:
-        phantom.act("terminate process", parameters=parameters, assets=['carbonblack'], callback=join_create_ticket_1, name="terminate_process_1")    
+        phantom.act("send email", parameters=parameters, assets=['smtp'], name="Send_Email_reverted", parent_action=action)    
     else:
-        phantom.error("'terminate_process_1' will not be executed due to lack of parameters")
-    
-    return
-
-def quarantine_device_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-
-    # collect data for 'quarantine_device_1' call
-    container_data = phantom.get_data("rkitdata", clear_data=True)
-
-    parameters = []
-    
-    # build parameters list for 'quarantine_device_1' call
-    for container_item in container_data:
-        if container_item[0]:
-            parameters.append({
-                'ip_hostname': container_item[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': container_item[1]},
-            })
-
-    if parameters:
-        phantom.act("quarantine device", parameters=parameters, assets=['carbonblack'], callback=get_system_info_1, name="quarantine_device_1")    
-    else:
-        phantom.error("'quarantine_device_1' will not be executed due to lack of parameters")
-    
-    return
-
-def get_system_info_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'get_system_info_1' call
-    inputs_data_1 = phantom.collect2(container=container, datapath=['quarantine_device_1:artifact:*.cef.sourceAddress', 'quarantine_device_1:artifact:*.id'], action_results=results)
-
-    parameters = []
-    
-    # build parameters list for 'get_system_info_1' call
-    for inputs_item_1 in inputs_data_1:
-        if inputs_item_1[0]:
-            parameters.append({
-                'ip_hostname': inputs_item_1[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': inputs_item_1[1]},
-            })
-
-    if parameters:
-        phantom.act("get system info", parameters=parameters, assets=['vmwarevsphere'], callback=decision_1, name="get_system_info_1", parent_action=action)    
-    else:
-        phantom.error("'get_system_info_1' will not be executed due to lack of parameters")
-    
-    return
-
-def get_system_info_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'get_system_info_2' call
-    filter_results_data_1 = phantom.collect2(container=container, datapath=["get_system_info_1:filtered-action_result.data.*.ip", "get_system_info_1:filtered-action_result.parameter.context.artifact_id"], action_results=filtered_results)
-
-    parameters = []
-    
-    # build parameters list for 'get_system_info_2' call
-    for filter_results_item_1 in filter_results_data_1:
-        if filter_results_item_1[0]:
-            parameters.append({
-                'ip_hostname': filter_results_item_1[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': filter_results_item_1[1]},
-            })
-
-    if parameters:
-        phantom.act("get system info", parameters=parameters, assets=['domainctrl1'], callback=disable_user_1, name="get_system_info_2")    
-    else:
-        phantom.error("'get_system_info_2' will not be executed due to lack of parameters")
-    
-    return
-
-def unquarantine_device_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'unquarantine_device_1' call
-    inputs_data_1 = phantom.collect2(container=container, datapath=['revert_vm_1:artifact:*.cef.sourceAddress', 'revert_vm_1:artifact:*.id'], action_results=results)
-
-    parameters = []
-    
-    # build parameters list for 'unquarantine_device_1' call
-    for inputs_item_1 in inputs_data_1:
-        if inputs_item_1[0]:
-            parameters.append({
-                'ip_hostname': inputs_item_1[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': inputs_item_1[1]},
-            })
-
-    if parameters:
-        phantom.act("unquarantine device", parameters=parameters, assets=['carbonblack'], callback=Send_Email_reverted, name="unquarantine_device_1", parent_action=action)    
-    else:
-        phantom.error("'unquarantine_device_1' will not be executed due to lack of parameters")
+        phantom.error("'Send_Email_reverted' will not be executed due to lack of parameters")
     
     return
 
