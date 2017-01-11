c525c2c19f0012402614af98d3ea961becc5c269
liondancer <bradfordli@hotmail.com>
2017-01-10 16:22:44 -0800

    Update tanium_app.py

--- tanium_app.py
+++ tanium_app.py
@@ -8,12 +8,9 @@ import json
 
 
 def list_questions_cb(action, success, incident, results, handle):
-
     if success:
         succ_results = phantom.get_successful_action_results_v2(results)
-
         results = succ_results[0]['data']
-
         for result in results:
             query = result['name']
             phantom.debug('query: {0}'.format(query))
@@ -21,31 +18,23 @@ def list_questions_cb(action, success, incident, results, handle):
             phantom.act('run query', parameters=params, assets=['tanium'], name="Query: {0}".format(query))
 
 def reboot_system_cb(action, success, container, results, handle):
-
     if success:
         phantom.set_action_limit(300) 
         phantom.act('list questions', parameters=[{ }], assets=["tanium"], callback=list_questions_cb)
 
 def terminate_process_cb(action, success, container, results, handle):
-
     if success:
         sourceAddress = set(phantom.collect(container, 'artifact:*.cef.sourceAddress'))
-
         parameters = []
-
         for ip_hostname in sourceAddress :
             parameters.append({
                 "ip_hostname": ip_hostname,
             })
-            
         phantom.act("reboot system", parameters=parameters, assets=["tanium"], callback=reboot_system_cb)
 
 def on_start(container):
-
     sourceAddress = set(phantom.collect(container, 'artifact:*.cef.sourceHostName'))
-
     parameters = []
-
     for ip_hostname in sourceAddress :
         parameters.append({
             "ip_hostname": ip_hostname,
