a3eac22b65452a41c876b76632a47914960e8fac
Philip Royer <proyer@splunk.com>
2018-05-22 11:45:39 -0700

    adjust "operates on" to "extrahop_events"

--- extrahop_externally_accessible_databases.py
+++ extrahop_externally_accessible_databases.py
@@ -18,60 +18,6 @@ def on_start(container):
     return
 
 """
-Retrieves a list of all of the protocols that a device communicated over the last 30 minutes
-"""
-def get_client_protocols(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('get_client_protocols() called')
-    
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'get_client_protocols' call
-    results_data_1 = phantom.collect2(container=container, datapath=['get_client_device_info:action_result.data.*.ipaddr4', 'get_client_device_info:action_result.data.*.id', 'get_client_device_info:action_result.parameter.context.artifact_id'], action_results=results)
-
-    parameters = []
-    
-    # build parameters list for 'get_client_protocols' call
-    for results_item_1 in results_data_1:
-        if results_item_1[0]:
-            parameters.append({
-                'ip': results_item_1[0],
-                'minutes': 30,
-                'eh_api_id': results_item_1[1],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': results_item_1[2]},
-            })
-
-    phantom.act("get protocols", parameters=parameters, assets=['extrahop'], name="get_client_protocols", parent_action=action)
-
-    return
-
-"""
-Block the source IP that is externally accessing an internal database on the Palo Alto Networks Firewall
-"""
-def block_client_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('block_client_ip() called')
-
-    # collect data for 'block_client_ip' call
-    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.sourceAddress', 'artifact:*.id'])
-
-    parameters = []
-    
-    # build parameters list for 'block_client_ip' call
-    for container_item in container_data:
-        if container_item[0]:
-            parameters.append({
-                'is_source_address': True,
-                'ip': container_item[0],
-                'vsys': "vsys1",
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': container_item[1]},
-            })
-
-    phantom.act("block ip", parameters=parameters, assets=['pan'], name="block_client_ip")
-
-    return
-
-"""
 Continue running this playbook only if the artifact label is "publicly_exposed_db"
 """
 def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
@@ -90,94 +36,6 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
         get_client_device_info(action=action, success=success, container=container, results=results, handle=handle)
         block_client_ip(action=action, success=success, container=container, results=results, handle=handle)
         return
-
-    return
-
-"""
-Retrieves a list of all of the peers that a device communicated with in the last 30 minutes
-"""
-def get_db_peers(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('get_db_peers() called')
-    
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'get_db_peers' call
-    results_data_1 = phantom.collect2(container=container, datapath=['get_db_device_info:action_result.data.*.ipaddr4', 'get_db_device_info:action_result.data.*.id', 'get_db_device_info:action_result.parameter.context.artifact_id'], action_results=results)
-
-    parameters = []
-    
-    # build parameters list for 'get_db_peers' call
-    for results_item_1 in results_data_1:
-        if results_item_1[0]:
-            parameters.append({
-                'ip': results_item_1[0],
-                'minutes': 30,
-                'peer_role': "any",
-                'protocol': "any",
-                'eh_api_id': results_item_1[1],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': results_item_1[2]},
-            })
-
-    phantom.act("get peers", parameters=parameters, assets=['extrahop'], name="get_db_peers", parent_action=action)
-
-    return
-
-"""
-Retrieves a list of all of the protocols that a device communicated over the last 30 minutes
-"""
-def get_db_protocols(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('get_db_protocols() called')
-    
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'get_db_protocols' call
-    results_data_1 = phantom.collect2(container=container, datapath=['get_db_device_info:action_result.data.*.ipaddr4', 'get_db_device_info:action_result.data.*.id', 'get_db_device_info:action_result.parameter.context.artifact_id'], action_results=results)
-
-    parameters = []
-    
-    # build parameters list for 'get_db_protocols' call
-    for results_item_1 in results_data_1:
-        if results_item_1[0]:
-            parameters.append({
-                'ip': results_item_1[0],
-                'minutes': 30,
-                'eh_api_id': results_item_1[1],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': results_item_1[2]},
-            })
-
-    phantom.act("get protocols", parameters=parameters, assets=['extrahop'], name="get_db_protocols", parent_action=action)
-
-    return
-
-"""
-Retrieves a list of all of the peers that a device communicated with in the last 30 minutes
-"""
-def get_client_peers(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('get_client_peers() called')
-    
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'get_client_peers' call
-    results_data_1 = phantom.collect2(container=container, datapath=['get_client_device_info:action_result.data.*.ipaddr4', 'get_client_device_info:action_result.data.*.id', 'get_client_device_info:action_result.parameter.context.artifact_id'], action_results=results)
-
-    parameters = []
-    
-    # build parameters list for 'get_client_peers' call
-    for results_item_1 in results_data_1:
-        if results_item_1[0]:
-            parameters.append({
-                'ip': results_item_1[0],
-                'minutes': 30,
-                'peer_role': "any",
-                'protocol': "any",
-                'eh_api_id': results_item_1[1],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': results_item_1[2]},
-            })
-
-    phantom.act("get peers", parameters=parameters, assets=['extrahop'], name="get_client_peers", parent_action=action)
 
     return
 
@@ -242,6 +100,148 @@ def get_client_device_info_callback(action=None, success=None, container=None, r
     
     get_client_peers(action=action, success=success, container=container, results=results, handle=handle)
     get_client_protocols(action=action, success=success, container=container, results=results, handle=handle)
+
+    return
+
+"""
+Retrieves a list of all of the peers that a device communicated with in the last 30 minutes
+"""
+def get_db_peers(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('get_db_peers() called')
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'get_db_peers' call
+    results_data_1 = phantom.collect2(container=container, datapath=['get_db_device_info:action_result.data.*.ipaddr4', 'get_db_device_info:action_result.data.*.id', 'get_db_device_info:action_result.parameter.context.artifact_id'], action_results=results)
+
+    parameters = []
+    
+    # build parameters list for 'get_db_peers' call
+    for results_item_1 in results_data_1:
+        if results_item_1[0]:
+            parameters.append({
+                'protocol': "any",
+                'ip': results_item_1[0],
+                'minutes': 30,
+                'peer_role': "any",
+                'eh_api_id': results_item_1[1],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': results_item_1[2]},
+            })
+
+    phantom.act("get peers", parameters=parameters, assets=['extrahop'], name="get_db_peers", parent_action=action)
+
+    return
+
+"""
+Retrieves a list of all of the protocols that a device communicated over the last 30 minutes
+"""
+def get_db_protocols(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('get_db_protocols() called')
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'get_db_protocols' call
+    results_data_1 = phantom.collect2(container=container, datapath=['get_db_device_info:action_result.data.*.ipaddr4', 'get_db_device_info:action_result.data.*.id', 'get_db_device_info:action_result.parameter.context.artifact_id'], action_results=results)
+
+    parameters = []
+    
+    # build parameters list for 'get_db_protocols' call
+    for results_item_1 in results_data_1:
+        if results_item_1[0]:
+            parameters.append({
+                'ip': results_item_1[0],
+                'minutes': 30,
+                'eh_api_id': results_item_1[1],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': results_item_1[2]},
+            })
+
+    phantom.act("get protocols", parameters=parameters, assets=['extrahop'], name="get_db_protocols", parent_action=action)
+
+    return
+
+"""
+Retrieves a list of all of the peers that a device communicated with in the last 30 minutes
+"""
+def get_client_peers(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('get_client_peers() called')
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'get_client_peers' call
+    results_data_1 = phantom.collect2(container=container, datapath=['get_client_device_info:action_result.data.*.ipaddr4', 'get_client_device_info:action_result.data.*.id', 'get_client_device_info:action_result.parameter.context.artifact_id'], action_results=results)
+
+    parameters = []
+    
+    # build parameters list for 'get_client_peers' call
+    for results_item_1 in results_data_1:
+        if results_item_1[0]:
+            parameters.append({
+                'protocol': "any",
+                'ip': results_item_1[0],
+                'minutes': 30,
+                'peer_role': "any",
+                'eh_api_id': results_item_1[1],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': results_item_1[2]},
+            })
+
+    phantom.act("get peers", parameters=parameters, assets=['extrahop'], name="get_client_peers", parent_action=action)
+
+    return
+
+"""
+Retrieves a list of all of the protocols that a device communicated over the last 30 minutes
+"""
+def get_client_protocols(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('get_client_protocols() called')
+    
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    
+    # collect data for 'get_client_protocols' call
+    results_data_1 = phantom.collect2(container=container, datapath=['get_client_device_info:action_result.data.*.ipaddr4', 'get_client_device_info:action_result.data.*.id', 'get_client_device_info:action_result.parameter.context.artifact_id'], action_results=results)
+
+    parameters = []
+    
+    # build parameters list for 'get_client_protocols' call
+    for results_item_1 in results_data_1:
+        if results_item_1[0]:
+            parameters.append({
+                'ip': results_item_1[0],
+                'minutes': 30,
+                'eh_api_id': results_item_1[1],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': results_item_1[2]},
+            })
+
+    phantom.act("get protocols", parameters=parameters, assets=['extrahop'], name="get_client_protocols", parent_action=action)
+
+    return
+
+"""
+Block the source IP that is externally accessing an internal database on the Palo Alto Networks Firewall
+"""
+def block_client_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('block_client_ip() called')
+
+    # collect data for 'block_client_ip' call
+    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.sourceAddress', 'artifact:*.id'])
+
+    parameters = []
+    
+    # build parameters list for 'block_client_ip' call
+    for container_item in container_data:
+        if container_item[0]:
+            parameters.append({
+                'is_source_address': True,
+                'ip': container_item[0],
+                'vsys': "vsys1",
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': container_item[1]},
+            })
+
+    phantom.act("block ip", parameters=parameters, assets=['pan'], name="block_client_ip")
 
     return
 
