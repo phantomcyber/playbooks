e01e345b0dddc87190e51086c916e7c4346ab8f5
Philip Royer <proyer@splunk.com>
2021-02-17 13:07:23 -0500

    extrahop_new_dns_servers py3 conversion

--- extrahop_new_dns_servers.py
+++ extrahop_new_dns_servers.py
@@ -8,7 +8,6 @@ This playbook gets initiated by a Timer asset (setup for the Timer asset is in t
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
-
 def on_start(container):
     phantom.debug('on_start() called')
     
@@ -20,7 +19,7 @@ def on_start(container):
 """
 Query ExtraHop for any new DNS servers that have been seen on your network in the last 30 minutes
 """
-def get_devices_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_devices_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('get_devices_1() called')
 
     # collect data for 'get_devices_1' call
@@ -33,18 +32,18 @@ def get_devices_1(action=None, success=None, container=None, results=None, handl
         'activity_type': "dns_server",
     })
 
-    phantom.act("get devices", parameters=parameters, assets=['extrahop'], callback=decision_1, name="get_devices_1")
+    phantom.act(action="get devices", parameters=parameters, assets=['extrahop'], callback=decision_1, name="get_devices_1")
 
     return
 
 """
 Only initiate a scan on returned devices that have an IP Address
 """
-def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('decision_1() called')
 
     # check for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
+    matched = phantom.decision(
         container=container,
         action_results=results,
         conditions=[
@@ -52,9 +51,9 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
         ])
 
     # call connected blocks if condition 1 matched
-    if matched_artifacts_1 or matched_results_1:
-        get_device_info_1(action=action, success=success, container=container, results=results, handle=handle)
-        list_nessus_policies(action=action, success=success, container=container, results=results, handle=handle)
+    if matched:
+        get_device_info_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
+        list_nessus_policies(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
         return
 
     return
@@ -62,9 +61,9 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 """
 Get more details about an ExtraHop device given its IP address. Details include MAC address, dhcp name, first discovered time, device type, and more
 """
-def get_device_info_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_device_info_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('get_device_info_1() called')
-    
+        
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     
     # collect data for 'get_device_info_1' call
@@ -81,24 +80,24 @@ def get_device_info_1(action=None, success=None, container=None, results=None, h
                 'context': {'artifact_id': results_item_1[1]},
             })
 
-    phantom.act("get device info", parameters=parameters, assets=['extrahop'], callback=get_device_info_1_callback, name="get_device_info_1")
+    phantom.act(action="get device info", parameters=parameters, assets=['extrahop'], callback=get_device_info_1_callback, name="get_device_info_1")
 
     return
 
-def get_device_info_1_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_device_info_1_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None):
     phantom.debug('get_device_info_1_callback() called')
     
-    get_peers_1(action=action, success=success, container=container, results=results, handle=handle)
-    get_protocols_1(action=action, success=success, container=container, results=results, handle=handle)
+    get_peers_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
+    get_protocols_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
 
     return
 
 """
 Retrieves a list of all of the protocols that a device communicated over the last 30 minutes
 """
-def get_protocols_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_protocols_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('get_protocols_1() called')
-    
+        
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     
     # collect data for 'get_protocols_1' call
@@ -117,16 +116,16 @@ def get_protocols_1(action=None, success=None, container=None, results=None, han
                 'context': {'artifact_id': results_item_1[2]},
             })
 
-    phantom.act("get protocols", parameters=parameters, assets=['extrahop'], name="get_protocols_1", parent_action=action)
+    phantom.act(action="get protocols", parameters=parameters, assets=['extrahop'], name="get_protocols_1", parent_action=action)
 
     return
 
 """
 Retrieves a list of all of the peers that a device communicated with in the last 30 minutes
 """
-def get_peers_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_peers_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('get_peers_1() called')
-    
+        
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     
     # collect data for 'get_peers_1' call
@@ -138,25 +137,25 @@ def get_peers_1(action=None, success=None, container=None, results=None, handle=
     for results_item_1 in results_data_1:
         if results_item_1[0]:
             parameters.append({
-                'protocol': "any",
                 'ip': results_item_1[0],
                 'minutes': 30,
-                'peer_role': "any",
+                'protocol': "any",
                 'eh_api_id': results_item_1[1],
+                'peer_role': "any",
                 # context (artifact id) is added to associate results with the artifact
                 'context': {'artifact_id': results_item_1[2]},
             })
 
-    phantom.act("get peers", parameters=parameters, assets=['extrahop'], name="get_peers_1", parent_action=action)
+    phantom.act(action="get peers", parameters=parameters, assets=['extrahop'], name="get_peers_1", parent_action=action)
 
     return
 
 """
 Initiate a Nessus scan on the new dns server
 """
-def scan_endpoint_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def scan_endpoint_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('scan_endpoint_1() called')
-    
+        
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     
     # collect data for 'scan_endpoint_1' call
@@ -168,15 +167,15 @@ def scan_endpoint_1(action=None, success=None, container=None, results=None, han
     # build parameters list for 'scan_endpoint_1' call
     for results_item_1 in results_data_1:
         for filtered_results_item_1 in filtered_results_data_1:
-            if results_item_1[0] and filtered_results_item_1[0]:
+            if filtered_results_item_1[0] and results_item_1[0]:
                 parameters.append({
-                    'target_to_scan': results_item_1[0],
                     'policy_id': filtered_results_item_1[0],
+                    'target_to_scan': results_item_1[0],
                     # context (artifact id) is added to associate results with the artifact
-                    'context': {'artifact_id': results_item_1[1]},
+                    'context': {'artifact_id': filtered_results_item_1[1]},
                 })
 
-    phantom.act("scan endpoint", parameters=parameters, assets=['nessus'], name="scan_endpoint_1")
+    phantom.act(action="scan endpoint", parameters=parameters, assets=['nessus scanner'], name="scan_endpoint_1")
 
     return
 
@@ -184,7 +183,7 @@ def scan_endpoint_1(action=None, success=None, container=None, results=None, han
 (Requires Configuration) Filter your Nessus policies by Name and determine the corresponding Nessus policy id.
 Fill this value in with the Name of the desired Nessus policy to scan the new dns server with.
 """
-def Nessus_Policy_Name(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def Nessus_Policy_Name(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('Nessus_Policy_Name() called')
 
     # collect filtered artifact ids for 'if' condition 1
@@ -198,28 +197,28 @@ def Nessus_Policy_Name(action=None, success=None, container=None, results=None, 
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        scan_endpoint_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        scan_endpoint_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     return
 
 """
 List the available Nessus scan policies
 """
-def list_nessus_policies(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def list_nessus_policies(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('list_nessus_policies() called')
-    
+        
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
     parameters = []
 
-    phantom.act("list policies", parameters=parameters, assets=['nessus'], callback=Nessus_Policy_Name, name="list_nessus_policies")
+    phantom.act(action="list policies", parameters=parameters, assets=['nessus scanner'], callback=Nessus_Policy_Name, name="list_nessus_policies")
 
     return
 
 def on_finish(container, summary):
     phantom.debug('on_finish() called')
     # This function is called after all actions are completed.
-    # summary of all the action and/or all detals of actions 
+    # summary of all the action and/or all details of actions
     # can be collected here.
 
     # summary_json = phantom.get_summary()
