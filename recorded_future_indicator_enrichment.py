feb0eb6fc3c9e9011c993cae92e7dd87358a023b
Philip Royer <proyer@splunk.com>
2021-02-17 13:16:57 -0500

    recorded_future_indicator_enrichment py3 conversion

--- recorded_future_indicator_enrichment.py
+++ recorded_future_indicator_enrichment.py
@@ -5,7 +5,6 @@ Enriches ingested events that contain file hashes, IP addresses, domain names, o
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
-
 ##############################
 # Start - Global Code Block
 
@@ -33,7 +32,7 @@ def on_start(container):
 """
 Filter common file hash and ip address fields
 """
-def filter_hash_and_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def filter_hash_and_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('filter_hash_and_ip() called')
 
     # collect filtered artifact ids for 'if' condition 1
@@ -46,7 +45,7 @@ def filter_hash_and_ip(action=None, success=None, container=None, results=None, 
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        ip_intel_source_address(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        ip_intel_source_address(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     # collect filtered artifact ids for 'if' condition 2
     matched_artifacts_2, matched_results_2 = phantom.condition(
@@ -58,7 +57,7 @@ def filter_hash_and_ip(action=None, success=None, container=None, results=None, 
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_2 or matched_results_2:
-        ip_intel_destination_address(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+        ip_intel_destination_address(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
 
     # collect filtered artifact ids for 'if' condition 3
     matched_artifacts_3, matched_results_3 = phantom.condition(
@@ -70,14 +69,14 @@ def filter_hash_and_ip(action=None, success=None, container=None, results=None, 
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_3 or matched_results_3:
-        file_intelligence_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_3, filtered_results=matched_results_3)
+        file_intelligence_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_3, filtered_results=matched_results_3)
 
     return
 
 """
 Filter the common domain name and URL fields
 """
-def filter_domain_and_url(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def filter_domain_and_url(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('filter_domain_and_url() called')
 
     # collect filtered artifact ids for 'if' condition 1
@@ -90,7 +89,7 @@ def filter_domain_and_url(action=None, success=None, container=None, results=Non
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        domain_intel_dest_dns(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        domain_intel_dest_dns(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     # collect filtered artifact ids for 'if' condition 2
     matched_artifacts_2, matched_results_2 = phantom.condition(
@@ -102,7 +101,7 @@ def filter_domain_and_url(action=None, success=None, container=None, results=Non
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_2 or matched_results_2:
-        url_intelligence_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+        url_intelligence_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
 
     # collect filtered artifact ids for 'if' condition 3
     matched_artifacts_3, matched_results_3 = phantom.condition(
@@ -114,14 +113,14 @@ def filter_domain_and_url(action=None, success=None, container=None, results=Non
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_3 or matched_results_3:
-        domain_intel_source_dns(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_3, filtered_results=matched_results_3)
+        domain_intel_source_dns(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_3, filtered_results=matched_results_3)
 
     return
 
 """
 Gather threat intelligence about source IP addresses in the event
 """
-def ip_intel_source_address(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def ip_intel_source_address(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('ip_intel_source_address() called')
 
     # collect data for 'ip_intel_source_address' call
@@ -138,14 +137,14 @@ def ip_intel_source_address(action=None, success=None, container=None, results=N
                 'context': {'artifact_id': filtered_artifacts_item_1[1]},
             })
 
-    phantom.act("ip intelligence", parameters=parameters, assets=['recorded_future'], name="ip_intel_source_address")
+    phantom.act(action="ip intelligence", parameters=parameters, assets=['recorded_future'], name="ip_intel_source_address")
 
     return
 
 """
 Gather threat intelligence about destination IP addresses in the event
 """
-def ip_intel_destination_address(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def ip_intel_destination_address(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('ip_intel_destination_address() called')
 
     # collect data for 'ip_intel_destination_address' call
@@ -162,14 +161,14 @@ def ip_intel_destination_address(action=None, success=None, container=None, resu
                 'context': {'artifact_id': filtered_artifacts_item_1[1]},
             })
 
-    phantom.act("ip intelligence", parameters=parameters, assets=['recorded_future'], name="ip_intel_destination_address")
+    phantom.act(action="ip intelligence", parameters=parameters, assets=['recorded_future'], name="ip_intel_destination_address")
 
     return
 
 """
 Gather threat intelligence about file hashes in the event
 """
-def file_intelligence_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def file_intelligence_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('file_intelligence_1() called')
 
     # collect data for 'file_intelligence_1' call
@@ -186,14 +185,14 @@ def file_intelligence_1(action=None, success=None, container=None, results=None,
                 'context': {'artifact_id': filtered_artifacts_item_1[1]},
             })
 
-    phantom.act("file intelligence", parameters=parameters, assets=['recorded_future'], name="file_intelligence_1")
+    phantom.act(action="file intelligence", parameters=parameters, assets=['recorded_future'], name="file_intelligence_1")
 
     return
 
 """
 Gather threat intelligence about source domain names in the event
 """
-def domain_intel_source_dns(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def domain_intel_source_dns(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('domain_intel_source_dns() called')
 
     # collect data for 'domain_intel_source_dns' call
@@ -210,14 +209,14 @@ def domain_intel_source_dns(action=None, success=None, container=None, results=N
                 'context': {'artifact_id': filtered_artifacts_item_1[1]},
             })
 
-    phantom.act("domain intelligence", parameters=parameters, assets=['recorded_future'], name="domain_intel_source_dns")
+    phantom.act(action="domain intelligence", parameters=parameters, assets=['recorded_future'], name="domain_intel_source_dns")
 
     return
 
 """
 Gather threat intelligence about destination domain names in the event
 """
-def domain_intel_dest_dns(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def domain_intel_dest_dns(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('domain_intel_dest_dns() called')
 
     # collect data for 'domain_intel_dest_dns' call
@@ -234,14 +233,14 @@ def domain_intel_dest_dns(action=None, success=None, container=None, results=Non
                 'context': {'artifact_id': filtered_artifacts_item_1[1]},
             })
 
-    phantom.act("domain intelligence", parameters=parameters, assets=['recorded_future'], name="domain_intel_dest_dns")
+    phantom.act(action="domain intelligence", parameters=parameters, assets=['recorded_future'], name="domain_intel_dest_dns")
 
     return
 
 """
 Gather threat intelligence about URLs in the event
 """
-def url_intelligence_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def url_intelligence_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('url_intelligence_1() called')
 
     # collect data for 'url_intelligence_1' call
@@ -258,14 +257,14 @@ def url_intelligence_1(action=None, success=None, container=None, results=None, 
                 'context': {'artifact_id': filtered_artifacts_item_1[1]},
             })
 
-    phantom.act("url intelligence", parameters=parameters, assets=['recorded_future'], name="url_intelligence_1")
+    phantom.act(action="url intelligence", parameters=parameters, assets=['recorded_future'], name="url_intelligence_1")
 
     return
 
 def on_finish(container, summary):
     phantom.debug('on_finish() called')
     # This function is called after all actions are completed.
-    # summary of all the action and/or all detals of actions 
+    # summary of all the action and/or all details of actions
     # can be collected here.
 
     # summary_json = phantom.get_summary()
