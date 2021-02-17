9cad079267605ce8a7eca89594c470797d8a8888
Philip Royer <proyer@splunk.com>
2021-02-17 13:07:24 -0500

    macos_ard_enumeration py3 conversion

--- macos_ard_enumeration.py
+++ macos_ard_enumeration.py
@@ -5,7 +5,6 @@ This helper playbook uses an nmap scan to build a custom list of endpoints runni
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
-
 def on_start(container):
     phantom.debug('on_start() called')
     
@@ -17,7 +16,7 @@ def on_start(container):
 """
 Scan the given subnet with a TCP syn-ack on port 5900 to detect VNC or ARD services.
 """
-def scan_port_5900(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def scan_port_5900(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('scan_port_5900() called')
 
     # collect data for 'scan_port_5900' call
@@ -26,21 +25,21 @@ def scan_port_5900(action=None, success=None, container=None, results=None, hand
     
     # build parameters list for 'scan_port_5900' call
     parameters.append({
-        'portlist': 5900,
-        'script-args': "",
         'script': "",
-        'ip_hostname': "198.51.100.*",
+        'portlist': 5900,
         'udp_scan': "",
+        'ip_hostname': "198.51.100.*",
+        'script-args': "",
     })
 
-    phantom.act("scan network", parameters=parameters, assets=['nmap'], callback=filter_1, name="scan_port_5900")
+    phantom.act(action="scan network", parameters=parameters, assets=['nmap'], callback=filter_1, name="scan_port_5900")
 
     return
 
 """
 Only pass on ip addresses that are found to have open ports.
 """
-def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('filter_1() called')
 
     # collect filtered artifact ids for 'if' condition 1
@@ -54,17 +53,17 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        add_addresses_to_list(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        add_addresses_to_list(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     return
 
 """
 Create a custom list with the given IP addresses or add to one if it exists.
 """
-def add_addresses_to_list(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def add_addresses_to_list(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('add_addresses_to_list() called')
 
-    filtered_results_data_1 = phantom.collect2(container=container, datapath=["filtered-data:filter_1:condition_1:scan_port_5900:action_result.data.*.addresses.ipv4.*.ip"])
+    filtered_results_data_1 = phantom.collect2(container=container, datapath=['filtered-data:filter_1:condition_1:scan_port_5900:action_result.data.*.addresses.ipv4.*.ip'])
 
     filtered_results_item_1_0 = [item[0] for item in filtered_results_data_1]
 
@@ -75,7 +74,7 @@ def add_addresses_to_list(action=None, success=None, container=None, results=Non
 def on_finish(container, summary):
     phantom.debug('on_finish() called')
     # This function is called after all actions are completed.
-    # summary of all the action and/or all detals of actions 
+    # summary of all the action and/or all details of actions
     # can be collected here.
 
     # summary_json = phantom.get_summary()
