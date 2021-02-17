0fb8a48750b76628752c21a98b893c19fa05d22a
Philip Royer <proyer@splunk.com>
2021-02-17 13:07:22 -0500

    customer_firewall_request_handle_artifact py3 conversion

--- customer_firewall_request_handle_artifact.py
+++ customer_firewall_request_handle_artifact.py
@@ -5,7 +5,6 @@ This playbook works in conjunction with "customer_firewall_request_parse_csv". A
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
-
 def on_start(container):
     phantom.debug('on_start() called')
     
@@ -17,7 +16,7 @@ def on_start(container):
 """
 Artifacts must have the label "customer_request" to be processed.
 """
-def filter_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def filter_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('filter_2() called')
 
     # collect filtered artifact ids for 'if' condition 1
@@ -30,14 +29,14 @@ def filter_2(action=None, success=None, container=None, results=None, handle=Non
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        filter_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        filter_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     return
 
 """
 Split processing based on "block_ip" vs. "unblock_ip" and "sourceAddress" vs. "destinationAddress".
 """
-def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('filter_1() called')
 
     # collect filtered artifact ids for 'if' condition 1
@@ -53,7 +52,7 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        block_src_ip(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        block_src_ip(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     # collect filtered artifact ids for 'if' condition 2
     matched_artifacts_2, matched_results_2 = phantom.condition(
@@ -68,7 +67,7 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_2 or matched_results_2:
-        block_dst_ip(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+        block_dst_ip(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
 
     # collect filtered artifact ids for 'if' condition 3
     matched_artifacts_3, matched_results_3 = phantom.condition(
@@ -83,7 +82,7 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_3 or matched_results_3:
-        unblock_src_ip(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_3, filtered_results=matched_results_3)
+        unblock_src_ip(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_3, filtered_results=matched_results_3)
 
     # collect filtered artifact ids for 'if' condition 4
     matched_artifacts_4, matched_results_4 = phantom.condition(
@@ -98,14 +97,14 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_4 or matched_results_4:
-        unblock_dst_ip(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_4, filtered_results=matched_results_4)
+        unblock_dst_ip(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_4, filtered_results=matched_results_4)
 
     return
 
 """
 Unblock traffic by source IP address.
 """
-def unblock_src_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def unblock_src_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('unblock_src_ip() called')
 
     # collect data for 'unblock_src_ip' call
@@ -116,21 +115,21 @@ def unblock_src_ip(action=None, success=None, container=None, results=None, hand
     # build parameters list for 'unblock_src_ip' call
     for filtered_artifacts_item_1 in filtered_artifacts_data_1:
         parameters.append({
-            'is_source_address': True,
             'ip': filtered_artifacts_item_1[0],
             'vsys': "",
+            'is_source_address': True,
             # context (artifact id) is added to associate results with the artifact
             'context': {'artifact_id': filtered_artifacts_item_1[1]},
         })
 
-    phantom.act("unblock ip", parameters=parameters, assets=['pan_firewall'], name="unblock_src_ip")
+    phantom.act(action="unblock ip", parameters=parameters, assets=['pan'], name="unblock_src_ip")
 
     return
 
 """
 Block traffic by destination IP address.
 """
-def block_dst_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def block_dst_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('block_dst_ip() called')
 
     # collect data for 'block_dst_ip' call
@@ -142,21 +141,21 @@ def block_dst_ip(action=None, success=None, container=None, results=None, handle
     for filtered_artifacts_item_1 in filtered_artifacts_data_1:
         if filtered_artifacts_item_1[0]:
             parameters.append({
-                'is_source_address': False,
                 'ip': filtered_artifacts_item_1[0],
                 'vsys': "",
+                'is_source_address': False,
                 # context (artifact id) is added to associate results with the artifact
                 'context': {'artifact_id': filtered_artifacts_item_1[1]},
             })
 
-    phantom.act("block ip", parameters=parameters, assets=['pan_firewall'], name="block_dst_ip")
+    phantom.act(action="block ip", parameters=parameters, assets=['pan'], name="block_dst_ip")
 
     return
 
 """
 Unblock traffic by destination IP address.
 """
-def unblock_dst_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def unblock_dst_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('unblock_dst_ip() called')
 
     # collect data for 'unblock_dst_ip' call
@@ -167,21 +166,21 @@ def unblock_dst_ip(action=None, success=None, container=None, results=None, hand
     # build parameters list for 'unblock_dst_ip' call
     for filtered_artifacts_item_1 in filtered_artifacts_data_1:
         parameters.append({
-            'is_source_address': False,
             'ip': filtered_artifacts_item_1[0],
             'vsys': "",
+            'is_source_address': False,
             # context (artifact id) is added to associate results with the artifact
             'context': {'artifact_id': filtered_artifacts_item_1[1]},
         })
 
-    phantom.act("unblock ip", parameters=parameters, assets=['pan_firewall'], name="unblock_dst_ip")
+    phantom.act(action="unblock ip", parameters=parameters, assets=['pan'], name="unblock_dst_ip")
 
     return
 
 """
 Block traffic by source IP address.
 """
-def block_src_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def block_src_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('block_src_ip() called')
 
     # collect data for 'block_src_ip' call
@@ -193,21 +192,21 @@ def block_src_ip(action=None, success=None, container=None, results=None, handle
     for filtered_artifacts_item_1 in filtered_artifacts_data_1:
         if filtered_artifacts_item_1[0]:
             parameters.append({
-                'is_source_address': True,
                 'ip': filtered_artifacts_item_1[0],
                 'vsys': "",
+                'is_source_address': True,
                 # context (artifact id) is added to associate results with the artifact
                 'context': {'artifact_id': filtered_artifacts_item_1[1]},
             })
 
-    phantom.act("block ip", parameters=parameters, assets=['pan_firewall'], name="block_src_ip")
+    phantom.act(action="block ip", parameters=parameters, assets=['pan'], name="block_src_ip")
 
     return
 
 def on_finish(container, summary):
     phantom.debug('on_finish() called')
     # This function is called after all actions are completed.
-    # summary of all the action and/or all detals of actions 
+    # summary of all the action and/or all details of actions
     # can be collected here.
 
     # summary_json = phantom.get_summary()
