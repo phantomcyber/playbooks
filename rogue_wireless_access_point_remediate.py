d54b53b65597c5cfe8af02c4f7c54f4d4fcf29e5
Philip Royer <proyer@splunk.com>
2021-02-17 13:16:58 -0500

    rogue_wireless_access_point_remediate py3 conversion

--- rogue_wireless_access_point_remediate.py
+++ rogue_wireless_access_point_remediate.py
@@ -1,11 +1,10 @@
 """
-This playbook helps the security team detect and remove rogue wireless access points (WAPs). A Raspberry Pi 3 with a battery pack is used as a mobile scanner, periodically polling the 2.4 GHz frequency and collecting the ESSID (network name), MAC address, channel, signal strength, signal quality, and security protocol of each WAP in range. The results of each scan are compared against a whitelist of known-good network names and a greylist of potential "evil twin" network names. The results are tracked live using artifacts and Heads-up Display pins posted to the most recently created Case with the label "wireless". Finally, a team member is tasked to carry around the Raspberry Pi and use the live feed of MAC addresses and signal strengths to locate the rogue WAPs and remove them.
+This playbook helps the security team detect and remove rogue wireless access points (WAPs). A Raspberry Pi 3 with a battery pack is used as a mobile scanner, periodically polling the 2.4 GHz frequency and collecting the ESSID (network name), MAC address, channel, signal strength, signal quality, and security protocol of each WAP in range. The results of each scan are compared against an allowlist of known-good network names and a potential list of potential "evil twin" network names. The results are tracked live using artifacts and Heads-up Display pins posted to the most recently created Case with the label "wireless". Finally, a team member is tasked to carry around the Raspberry Pi and use the live feed of MAC addresses and signal strengths to locate the rogue WAPs and remove them.
 """
 
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
-
 ##############################
 # Start - Global Code Block
 
@@ -148,7 +147,7 @@ Join together the execution paths of the SSH command and the HTTP query, then ca
 
 If using a different version of iwlist or using a different scanning tool, the function parse_and_save_iwlist() may need to be modified.
 """
-def collect_data(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def collect_data(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('collect_data() called')
     
     iwlist = phantom.collect2(container=container, datapath=['execute_program_1:action_result.data.*.output',
@@ -159,15 +158,15 @@ def collect_data(action=None, success=None, container=None, results=None, handle
     live_case_body = phantom.collect2(container=container, datapath=['find_case:action_result.data.*.response_body'], action_results=results)
     parse_and_save_live_case(live_case_body)
 
-    check_whitelist(container=container)
+    check_allowlist(container=container)
     
     return
 
-def join_collect_data(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def join_collect_data(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None):
     phantom.debug('join_collect_data() called')
 
-    # check if all connected incoming actions are done i.e. have succeeded or failed
-    if phantom.actions_done([ 'execute_program_1', 'find_case' ]):
+    # check if all connected incoming playbooks, actions, or custom functions are done i.e. have succeeded or failed
+    if phantom.completed(action_names=['execute_program_1', 'find_case']):
         
         # call connected block "collect_data"
         collect_data(container=container, handle=handle)
@@ -175,42 +174,42 @@ def join_collect_data(action=None, success=None, container=None, results=None, h
     return
 
 """
-Check the results of the WiFi scan against our whitelist of official company networks which are saved as a Custom List called "Example Company WiFi ESSID Whitelist". Stop investigating any networks that are both on the whitelist and are using WPA2.
+Check the results of the WiFi scan against our allowlist of official company networks which are saved as a Custom List called "Example Company WiFi ESSID Allowlist". Stop investigating any networks that are both on the allowlist and are using WPA2.
 
-In this case we are not pursuing further investigation of WPA2 networks that match the whitelist because our client devices will not automatically connect to them.
+In this case we are not pursuing further investigation of WPA2 networks that match the allowlist because our client devices will not automatically connect to them.
 """
-def check_whitelist(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('check_whitelist() called')
+def check_allowlist(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('check_allowlist() called')
     
     parsed_access_points = json.loads(phantom.get_run_data(key='parsed_access_points'))
     
-    success, message, whitelist = phantom.get_list(list_name='Example Company WiFi ESSID Whitelist')
+    success, message, allowlist = phantom.get_list(list_name='Example Company WiFi ESSID Allowlist')
     
-    whitelist_filtered_access_points = [
+    allowlist_filtered_access_points = [
         ap for ap in parsed_access_points 
         if (
-            # all ESSIDs that aren't in the whitelist are suspicious
-            [ap["ESSID"]] not in whitelist
+            # all ESSIDs that aren't in the allowlist are suspicious
+            [ap["ESSID"]] not in allowlist
             or
             # all non-WPA2 access points are suspicious
             'WPA2' not in ap['security_protocol']
         )
     ]
 
-    message = 'out of the {} access points identified by the scan, {} matched the whitelist and are being ignored'.format(len(parsed_access_points), len(parsed_access_points) - len(whitelist_filtered_access_points))
+    message = 'out of the {} access points identified by the scan, {} matched the allowlist and are being ignored'.format(len(parsed_access_points), len(parsed_access_points) - len(allowlist_filtered_access_points))
     phantom.debug(message)
     live_comment(message)
     
-    phantom.save_run_data(value=json.dumps(whitelist_filtered_access_points), key='whitelist_filtered_access_points')
+    phantom.save_run_data(value=json.dumps(allowlist_filtered_access_points), key='allowlist_filtered_access_points')
     
-    check_greylist() 
+    check_potential_list() 
     
     return
 
 """
 Retrieve a list of all the cards currently pinned to the Heads-up Display of the Case that will be used to track the results.
 """
-def get_pins(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_pins(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('get_pins() called')
     
     live_case = phantom.get_run_data(key='live_case_id')
@@ -229,9 +228,108 @@ def get_pins(action=None, success=None, container=None, results=None, handle=Non
     return
 
 """
+Only proceed to assign a task if the Case has no owner. This prevents duplicate task assignments while the responder is actively completing the task.
+"""
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('decision_1() called')
+
+    live_case_owner = phantom.get_run_data(key='live_case_owner')
+    live_case_owner = json.loads(live_case_owner)
+    
+    # only task a responder if there is no owner yet
+    if not live_case_owner:
+        find_and_disable_rogue_ap()
+
+    return
+
+"""
+Use the HTTP app to query the REST API of this Phantom instance for the most recently created Case with the label Wireless. This is the Case that will be updated with new artifacts and Heads-up Display information containing the results of the WiFi scan.
+
+When testing and using this playbook it may be helpful to open up this Case in Mission Control to monitor the output.
+"""
+def find_case(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('find_case() called')
+
+    # collect data for 'find_case' call
+
+    parameters = []
+    
+    # build parameters list for 'find_case' call
+    parameters.append({
+        'headers': "",
+        'location': "rest/container?_filter_label=\"wireless\"&_filter_container_type=\"case\"&sort=start_time&order=desc&page_size=1",
+        'verify_certificate': False,
+    })
+
+    phantom.act(action="get data", parameters=parameters, assets=['http'], callback=join_collect_data, name="find_case")
+
+    return
+
+"""
+Check the remaining access points against a potential list of several possible network names that an attacker might use to spoof an official company network. The potential list is loaded from the Custom List called "Potential Rogue Access Point ESSIDs". To account for small variations in network names, a Levenshtein edit distance is used so even non-exact matches will be considered suspicious.
+
+The resulting artifact saved to the Case will have a field called "matched_rule", which will show the first entry from the Custom List that matches the given network name.
+"""
+def check_potential_list(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('check_potential_list() called')
+    
+    edit_distance_threshold = 5
+    
+    success, message, potentials = phantom.get_list(list_name='Potential Rogue Access Point ESSIDs')
+    allowlist_filtered_access_points = json.loads(phantom.get_run_data(key='allowlist_filtered_access_points'))
+    
+    scanned_ESSIDs = [ap['ESSID'] for ap in allowlist_filtered_access_points]
+    
+    # compare each ESSID against each potential evil twin and escalate those with a sufficiently small edit distance
+    matches = 0
+    for ap in allowlist_filtered_access_points:
+        ap['is_escalated'] = False
+        ap['matched_rule'] = None
+        for potential in potentials:
+            if edit_distance(ap['ESSID'], potential[0]) < edit_distance_threshold:
+                ap['is_escalated'] = True
+                ap['matched_rule'] = potential
+                matches += 1
+                break
+
+    message = '{} out of {} access points fuzzy-matched "Potential Rogue Access Point ESSIDs"'.format(
+        matches, len(allowlist_filtered_access_points))
+    phantom.debug(message)
+    live_comment(message)
+
+    phantom.save_run_data(value=json.dumps(allowlist_filtered_access_points), key='fuzzy_matched_access_points')
+
+    get_pins()
+    
+    return
+
+"""
+Scan all WiFi access points in range of the Raspberry Pi using the command "iwlist wlan0 scanning". The following information will be returned for each access point: ESSID (network name), MAC address, channel, signal strength, signal quality, and security protocol.
+
+If adapting to scan from a Windows machine, "netsh.exe" should be able to list access points in range.
+"""
+def execute_program_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('execute_program_1() called')
+
+    # collect data for 'execute_program_1' call
+
+    parameters = []
+    
+    # build parameters list for 'execute_program_1' call
+    parameters.append({
+        'command': "sudo /sbin/iwlist wlan0 scanning",
+        'timeout': "",
+        'ip_hostname': "192.168.1.100",
+    })
+
+    phantom.act(action="execute program", parameters=parameters, assets=['raspberry_pi_ssh'], callback=join_collect_data, name="execute_program_1")
+
+    return
+
+"""
 Update the Case with artifacts and Heads-up Display cards containing the results of this playbook run. This overwrites all existing Heads-up Display cards to keep the signal strength values fresh and prevent duplicates, but the artifacts will not be deleted so they will be sortable by timestamp for a historical record.
 """
-def update_case(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def update_case(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('update_case() called')
     
     get_pins_body = phantom.collect2(container=container, datapath=['get_pins:action_result.data.*.response_body'], action_results=results)
@@ -244,7 +342,7 @@ def update_case(action=None, success=None, container=None, results=None, handle=
     for pin in get_pins_body[0][0]['data']:
         phantom.delete_pin(int(pin['id']))
     
-    # add a pin for each ap that fuzzy-matched the greylist
+    # add a pin for each ap that fuzzy-matched the potential list
     for ap in fuzzy_matched_access_points:
         if ap['is_escalated']:
             phantom.pin(container=live_case_id, message=ap['signal_strength'], data=ap['mac_address'], pin_type="card_medium", pin_style="red")
@@ -263,85 +361,9 @@ def update_case(action=None, success=None, container=None, results=None, handle=
     return
 
 """
-Only proceed to assign a task if the Case has no owner. This prevents duplicate task assignments while the responder is actively completing the task.
-"""
-def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('decision_1() called')
-
-    live_case_owner = phantom.get_run_data(key='live_case_owner')
-    live_case_owner = json.loads(live_case_owner)
-    
-    # only task a responder if there is no owner yet
-    if not live_case_owner:
-        find_and_disable_rogue_ap()
-
-    return
-
-"""
-Use the HTTP app to query the REST API of this Phantom instance for the most recently created Case with the label Wireless. This is the Case that will be updated with new artifacts and Heads-up Display information containing the results of the WiFi scan.
-
-When testing and using this playbook it may be helpful to open up this Case in Mission Control to monitor the output.
-"""
-def find_case(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('find_case() called')
-
-    # collect data for 'find_case' call
-
-    parameters = []
-    
-    # build parameters list for 'find_case' call
-    parameters.append({
-        'headers': "",
-        'location': "rest/container?_filter_label=\"wireless\"&_filter_container_type=\"case\"&sort=start_time&order=desc&page_size=1",
-        'verify_certificate': False,
-    })
-
-    phantom.act("get data", parameters=parameters, assets=['http'], callback=join_collect_data, name="find_case")
-
-    return
-
-"""
-Check the remaining access points against a greylist of several possible network names that an attacker might use to spoof an official company network. The greylist is loaded from the Custom List called "Potential Rogue Access Point ESSIDs". To account for small variations in network names, a Levenshtein edit distance is used so even non-exact matches will be considered suspicious.
-
-The resulting artifact saved to the Case will have a field called "matched_rule", which will show the first entry from the Custom List that matches the given network name.
-"""
-def check_greylist(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('check_greylist() called')
-    
-    edit_distance_threshold = 5
-    
-    success, message, potentials = phantom.get_list(list_name='Potential Rogue Access Point ESSIDs')
-    whitelist_filtered_access_points = json.loads(phantom.get_run_data(key='whitelist_filtered_access_points'))
-    
-    scanned_ESSIDs = [ap['ESSID'] for ap in whitelist_filtered_access_points]
-    
-    # compare each ESSID against each potential evil twin and escalate those with a sufficiently small edit distance
-    matches = 0
-    for ap in whitelist_filtered_access_points:
-        ap['is_escalated'] = False
-        ap['matched_rule'] = None
-        for potential in potentials:
-            if edit_distance(ap['ESSID'], potential[0]) < edit_distance_threshold:
-                ap['is_escalated'] = True
-                ap['matched_rule'] = potential
-                matches += 1
-                break
-
-    message = '{} out of {} access points fuzzy-matched "Potential Rogue Access Point ESSIDs"'.format(
-        matches, len(whitelist_filtered_access_points))
-    phantom.debug(message)
-    live_comment(message)
-
-    phantom.save_run_data(value=json.dumps(whitelist_filtered_access_points), key='fuzzy_matched_access_points')
-
-    get_pins()
-    
-    return
-
-"""
 Assign a manual task for an operator to remediate. The HUD can be used to live-track the MAC addresses and signal strengths of the identified access points.
 """
-def find_and_disable_rogue_ap(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def find_and_disable_rogue_ap(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('find_and_disable() called')
     
     live_case_id = phantom.get_run_data(key='live_case_id')
@@ -357,33 +379,10 @@ def find_and_disable_rogue_ap(action=None, success=None, container=None, results
 
     return
 
-"""
-Scan all WiFi access points in range of the Raspberry Pi using the command "iwlist wlan0 scanning". The following information will be returned for each access point: ESSID (network name), MAC address, channel, signal strength, signal quality, and security protocol.
-
-If adapting to scan from a Windows machine, "netsh.exe" should be able to list access points in range.
-"""
-def execute_program_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('execute_program_1() called')
-
-    # collect data for 'execute_program_1' call
-
-    parameters = []
-    
-    # build parameters list for 'execute_program_1' call
-    parameters.append({
-        'command': "sudo /sbin/iwlist wlan0 scanning",
-        'timeout': "",
-        'ip_hostname': "192.168.1.100",
-    })
-
-    phantom.act("execute program", parameters=parameters, assets=['raspberry_pi_ssh'], callback=join_collect_data, name="execute_program_1")
-
-    return
-
 def on_finish(container, summary):
     phantom.debug('on_finish() called')
     # This function is called after all actions are completed.
-    # summary of all the action and/or all detals of actions 
+    # summary of all the action and/or all details of actions
     # can be collected here.
 
     # summary_json = phantom.get_summary()
