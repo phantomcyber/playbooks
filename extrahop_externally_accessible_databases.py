4720205477a1505697f4cb376ad145c636be7729
Philip Royer <proyer@splunk.com>
2021-02-17 13:07:23 -0500

    extrahop_externally_accessible_databases py3 conversion

--- extrahop_externally_accessible_databases.py
+++ extrahop_externally_accessible_databases.py
@@ -8,7 +8,6 @@ This playbook processes an ExtraHop detection of an internal database being acce
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
-
 def on_start(container):
     phantom.debug('on_start() called')
     
@@ -20,21 +19,21 @@ def on_start(container):
 """
 Continue running this playbook only if the artifact label is "publicly_exposed_db"
 """
-def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('decision_1() called')
 
     # check for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
+    matched = phantom.decision(
         container=container,
         conditions=[
             ["artifact:*.label", "==", "publicly_exposed_db"],
         ])
 
     # call connected blocks if condition 1 matched
-    if matched_artifacts_1 or matched_results_1:
-        get_db_device_info(action=action, success=success, container=container, results=results, handle=handle)
-        get_client_device_info(action=action, success=success, container=container, results=results, handle=handle)
-        block_client_ip(action=action, success=success, container=container, results=results, handle=handle)
+    if matched:
+        get_db_device_info(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
+        get_client_device_info(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
+        block_client_ip(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
         return
 
     return
@@ -42,7 +41,7 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 """
 Get more details about the internal database server given its IP address. Details include MAC address, dhcp name, first discovered time, device type, and more
 """
-def get_db_device_info(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_db_device_info(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('get_db_device_info() called')
 
     # collect data for 'get_db_device_info' call
@@ -59,22 +58,22 @@ def get_db_device_info(action=None, success=None, container=None, results=None, 
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act("get device info", parameters=parameters, assets=['extrahop'], callback=get_db_device_info_callback, name="get_db_device_info")
+    phantom.act(action="get device info", parameters=parameters, assets=['extrahop'], callback=get_db_device_info_callback, name="get_db_device_info")
 
     return
 
-def get_db_device_info_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_db_device_info_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None):
     phantom.debug('get_db_device_info_callback() called')
     
-    get_db_peers(action=action, success=success, container=container, results=results, handle=handle)
-    get_db_protocols(action=action, success=success, container=container, results=results, handle=handle)
+    get_db_peers(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
+    get_db_protocols(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
 
     return
 
 """
 Get more details about the external client given its IP address. Details include MAC address, dhcp name, first discovered time, device type, and more
 """
-def get_client_device_info(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_client_device_info(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('get_client_device_info() called')
 
     # collect data for 'get_client_device_info' call
@@ -91,24 +90,24 @@ def get_client_device_info(action=None, success=None, container=None, results=No
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act("get device info", parameters=parameters, assets=['extrahop'], callback=get_client_device_info_callback, name="get_client_device_info")
+    phantom.act(action="get device info", parameters=parameters, assets=['extrahop'], callback=get_client_device_info_callback, name="get_client_device_info")
 
     return
 
-def get_client_device_info_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_client_device_info_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None):
     phantom.debug('get_client_device_info_callback() called')
     
-    get_client_peers(action=action, success=success, container=container, results=results, handle=handle)
-    get_client_protocols(action=action, success=success, container=container, results=results, handle=handle)
+    get_client_peers(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
+    get_client_protocols(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
 
     return
 
 """
 Retrieves a list of all of the peers that a device communicated with in the last 30 minutes
 """
-def get_db_peers(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_db_peers(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('get_db_peers() called')
-    
+        
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     
     # collect data for 'get_db_peers' call
@@ -120,25 +119,25 @@ def get_db_peers(action=None, success=None, container=None, results=None, handle
     for results_item_1 in results_data_1:
         if results_item_1[0]:
             parameters.append({
-                'protocol': "any",
                 'ip': results_item_1[0],
                 'minutes': 30,
-                'peer_role': "any",
+                'protocol': "any",
                 'eh_api_id': results_item_1[1],
+                'peer_role': "any",
                 # context (artifact id) is added to associate results with the artifact
                 'context': {'artifact_id': results_item_1[2]},
             })
 
-    phantom.act("get peers", parameters=parameters, assets=['extrahop'], name="get_db_peers", parent_action=action)
+    phantom.act(action="get peers", parameters=parameters, assets=['extrahop'], name="get_db_peers", parent_action=action)
 
     return
 
 """
 Retrieves a list of all of the protocols that a device communicated over the last 30 minutes
 """
-def get_db_protocols(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_db_protocols(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('get_db_protocols() called')
-    
+        
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     
     # collect data for 'get_db_protocols' call
@@ -157,16 +156,16 @@ def get_db_protocols(action=None, success=None, container=None, results=None, ha
                 'context': {'artifact_id': results_item_1[2]},
             })
 
-    phantom.act("get protocols", parameters=parameters, assets=['extrahop'], name="get_db_protocols", parent_action=action)
+    phantom.act(action="get protocols", parameters=parameters, assets=['extrahop'], name="get_db_protocols", parent_action=action)
 
     return
 
 """
 Retrieves a list of all of the peers that a device communicated with in the last 30 minutes
 """
-def get_client_peers(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_client_peers(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('get_client_peers() called')
-    
+        
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     
     # collect data for 'get_client_peers' call
@@ -178,25 +177,25 @@ def get_client_peers(action=None, success=None, container=None, results=None, ha
     for results_item_1 in results_data_1:
         if results_item_1[0]:
             parameters.append({
-                'protocol': "any",
                 'ip': results_item_1[0],
                 'minutes': 30,
-                'peer_role': "any",
+                'protocol': "any",
                 'eh_api_id': results_item_1[1],
+                'peer_role': "any",
                 # context (artifact id) is added to associate results with the artifact
                 'context': {'artifact_id': results_item_1[2]},
             })
 
-    phantom.act("get peers", parameters=parameters, assets=['extrahop'], name="get_client_peers", parent_action=action)
+    phantom.act(action="get peers", parameters=parameters, assets=['extrahop'], name="get_client_peers", parent_action=action)
 
     return
 
 """
 Retrieves a list of all of the protocols that a device communicated over the last 30 minutes
 """
-def get_client_protocols(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def get_client_protocols(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('get_client_protocols() called')
-    
+        
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     
     # collect data for 'get_client_protocols' call
@@ -215,14 +214,14 @@ def get_client_protocols(action=None, success=None, container=None, results=None
                 'context': {'artifact_id': results_item_1[2]},
             })
 
-    phantom.act("get protocols", parameters=parameters, assets=['extrahop'], name="get_client_protocols", parent_action=action)
+    phantom.act(action="get protocols", parameters=parameters, assets=['extrahop'], name="get_client_protocols", parent_action=action)
 
     return
 
 """
 Block the source IP that is externally accessing an internal database on the Palo Alto Networks Firewall
 """
-def block_client_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+def block_client_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug('block_client_ip() called')
 
     # collect data for 'block_client_ip' call
@@ -234,21 +233,21 @@ def block_client_ip(action=None, success=None, container=None, results=None, han
     for container_item in container_data:
         if container_item[0]:
             parameters.append({
-                'is_source_address': True,
                 'ip': container_item[0],
                 'vsys': "vsys1",
+                'is_source_address': True,
                 # context (artifact id) is added to associate results with the artifact
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act("block ip", parameters=parameters, assets=['pan'], name="block_client_ip")
+    phantom.act(action="block ip", parameters=parameters, assets=['pan'], name="block_client_ip")
 
     return
 
 def on_finish(container, summary):
     phantom.debug('on_finish() called')
     # This function is called after all actions are completed.
-    # summary of all the action and/or all detals of actions 
+    # summary of all the action and/or all details of actions
     # can be collected here.
 
     # summary_json = phantom.get_summary()
