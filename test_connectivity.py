098e65258f94b82f4bb1190bcad34a968282e237
Philip Royer <proyer@splunk.com>
2021-02-18 13:59:07 -0500

    test_connectivity comment update for refresh

--- test_connectivity.py
+++ test_connectivity.py
@@ -14,6 +14,7 @@ def get_report_cb(action, success, incident, results, handle):
 
 def on_start(incident):
 
+    # run test connectivity on all available assets
     phantom.act('test connectivity', parameters=[], callback=get_report_cb)
 
     return
