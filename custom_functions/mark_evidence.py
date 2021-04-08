1aa7d7b6aa825885d908fd713a6d0dc8396d545c
kelby-shelton <kelbys@splunk.com>
2021-04-08 12:36:08 -0500

    Added support for results object

--- custom_functions/mark_evidence.py
+++ custom_functions/mark_evidence.py
@@ -1,10 +1,10 @@
-def mark_evidence(container=None, object_id=None, content_type=None, **kwargs):
+def mark_evidence(container=None, input_object=None, content_type=None, **kwargs):
     """
     Mark an object as Evidence in a container
     
     Args:
         container (CEF type: phantom container id): Container ID or Container Object
-        object_id (CEF type: *): Id of object to be added - artifact id, note id, etc.
+        input_object (CEF type: *): Id or object to be added - artifact id, note id, etc.
         content_type (CEF type: *): The content type of the object to add as evidence which must be one of the following:
                         
                         vault_id
@@ -14,33 +14,21 @@ def mark_evidence(container=None, object_id=None, content_type=None, **kwargs):
                         note
     
     Returns a JSON-serializable object that implements the configured data paths:
-        id (CEF type: *): ID of the evidence item
+        *.id (CEF type: *): ID of the evidence item
     """
     ############################ Custom Code Goes Below This Line #################################
     import json
     import phantom.rules as phantom
     
-    outputs = {}
+    outputs = []
     container_id = None
+    data = []
     valid_types = ['vault_id','artifact','actionrun','container','note']
     
     # Ensure valid content_type: 
     if content_type.lower() not in valid_types:
         raise TypeError(f"The content_type '{content_type}' is not a valid content_type")
     
-    # If vault_id was entered, check to see if user already has a vault integer
-    # else if user entered a hash vault_id, attempt to translate to a vault integer
-    if content_type.lower() == 'vault_id':
-        if isinstance(object_id, int):
-            content_type = "containerattachment"
-        else:
-            success, message, info = phantom.vault_info(vault_id=object_id)
-            if success == False:
-                raise RuntimeError(f"Invalid vault_id: {message}")
-            else:
-                object_id = info[0]['id']    
-                content_type = "containerattachment"
-
     # Ensure valid container input
     if isinstance(container, dict) and container.get('id'):
         container_id = container['id']
@@ -49,29 +37,68 @@ def mark_evidence(container=None, object_id=None, content_type=None, **kwargs):
     else:
         raise TypeError("The input 'container' is neither a container dictionary nor an int, so it cannot be used")
     
-    # Build json dictionary for requests post
-    data = {
-        "container_id": container_id,
-        "object_id": object_id,
-        "content_type": content_type,
-        }
+    # If content added is type 'action run',
+    # then iterate through an input object that is a results object,
+    # and append the action_run_id's to data
+    if isinstance(input_object, list) and content_type == 'actionrun':
+        for action_result in input_object:
+            phantom.debug(action_result)
+            if action_result.get('action_run_id'):
+                data.append({
+                    "container_id": container_id,
+                    "object_id": action_result['action_run_id'],
+                    "content_type": content_type,
+                })
+        # If data is still an empty list after for loop, 
+        # it indicates that the input_object was not a valid results object
+        if not data:
+            raise TypeError("The input for 'object_id' is not a valid integer or supported object.")
+            
+    # If vault_id was entered, check to see if user already entered a vault integer
+    # else if user entered a hash vault_id, attempt to translate to a vault integer            
+    elif input_object and content_type.lower() == 'vault_id':
+        if isinstance(input_object, int):
+            content_type = "containerattachment"
+        else:
+            success, message, info = phantom.vault_info(vault_id=input_object)
+            if success == False:
+                raise RuntimeError(f"Invalid vault_id: {message}")
+            else:
+                input_object = info[0]['id']    
+                content_type = "containerattachment"
+        data = [{
+            "container_id": container_id,
+            "object_id": input_object,
+            "content_type": content_type,
+            }]
+    
+    # If input_object is an integer, it is assumed that its a valid input_object
+    elif isinstance(input_object, int):
+        data = [{
+            "container_id": container_id,
+            "object_id": input_object,
+            "content_type": content_type,
+            }]    
+    else:
+        raise TypeError(f"The input_object is not a valid integer or supported object. Type '{type(input_object)}'")
     
     # Build url for evidence endpoint
     url = phantom.build_phantom_rest_url('evidence')
     
     # Post data to evidence endpoint
-    response = phantom.requests.post(uri=url, json=data, verify=False).json()
-    
-    # If successful add evidence id to outputs
-    # elif evidence already exists print to debug
-    # else error out 
-    if response.get('success'):
-        outputs['id'] = response['id']
-    elif response.get('failed') and response.get('message') == 'Already added to Evidence.':
-        phantom.debug(f"{content_type} \'{container_id}\' {response['message']}")
-    else:
-        raise RuntimeError(f"Unable to add evidence: {response}")
-        
+    for item in data:
+        response = phantom.requests.post(uri=url, json=item, verify=False).json()
+
+        # If successful add evidence id to outputs
+        # elif evidence already exists print to debug
+        # else error out 
+        if response.get('success'):
+            outputs.append({'id': response['id']})
+        elif response.get('failed') and response.get('message') == 'Already added to Evidence.':
+            phantom.debug(f"{content_type} \'{container_id}\' {response['message']}")
+        else:
+            raise RuntimeError(f"Unable to add evidence: {response}")
+
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
     return outputs
