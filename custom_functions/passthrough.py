4a534956fc464b51c5de2f0faa93fd60ef451df8
Philip Royer <proyer@splunk.com>
2021-04-16 15:21:58 -0400

    update python version of passthrough custom function

--- custom_functions/passthrough.py
+++ custom_functions/passthrough.py
@@ -25,13 +25,21 @@ def passthrough(input_1=None, input_2=None, input_3=None, input_4=None, input_5=
     outputs = []
     for index, input_value in enumerate([input_1, input_2, input_3, input_4, input_5, input_6, input_7, input_8, input_9, input_10]):
         if input_value:
-            phantom.debug("handling input: {}".format(input_value))
-            this_output = {}
-            this_output['item'] = input_value
-            this_output['input_name'] = "input_{}".format(index+1) 
-            outputs.append(this_output)
+            if isinstance(input_value, list):
+                for input_item in input_value:
+                    this_output = {}
+                    this_output['item'] = input_item
+                    this_output['input_name'] = "input_{}".format(index+1) 
+                    outputs.append(this_output)
+                
+            else:
+                this_output = {}
+                this_output['item'] = input_value
+                this_output['input_name'] = "input_{}".format(index+1) 
+                outputs.append(this_output)
 
-    phantom.debug(outputs)
+    phantom.debug('outputs of passthrough:\n{}'.format(outputs))
+
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
     return outputs
