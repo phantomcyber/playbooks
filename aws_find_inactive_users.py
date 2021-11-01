29fa4d04ef0661c3abfd246c53a8564bf07ea46a
Philip Royer <proyer@splunk.com>
2021-11-01 16:44:33 -0400

    update aws_find_inactive_users for new vpe and child playbook inputs

--- aws_find_inactive_users.py
+++ aws_find_inactive_users.py
@@ -2,44 +2,34 @@
 Find AWS accounts that have not been used for a long time (90 days by default). For each unused account, gather additional group and policy information and create an artifact to enable further automation or manual action.
 """
 
+
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
+
+
 def on_start(container):
     phantom.debug('on_start() called')
-    
-    # call 'calculate_start_time' block
-    calculate_start_time(container=container)
 
     # call 'list_all_accounts' block
     list_all_accounts(container=container)
+    # call 'calculate_start_time' block
+    calculate_start_time(container=container)
 
     return
 
-"""
-Calculate the time to compare against in the filter for unused accounts.
-"""
-def calculate_start_time(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('calculate_start_time() called')
-    
-    literal_values_0 = [
-        [
-            -90,
-            "days",
-            "%Y-%m-%dT%H:%M:%SZ",
-        ],
-    ]
+def list_all_accounts(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("list_all_accounts() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    ################################################################################
+    # List all AWS IAM accounts, which will include the PasswordLastUsed field for 
+    # us to filter on.
+    ################################################################################
 
     parameters = []
 
-    for item0 in literal_values_0:
-        parameters.append({
-            'input_datetime': None,
-            'amount_to_modify': item0[0],
-            'modification_unit': item0[1],
-            'input_format_string': None,
-            'output_format_string': item0[2],
-        })
     ################################################################################
     ## Custom Code Start
     ################################################################################
@@ -48,129 +38,185 @@ def calculate_start_time(action=None, success=None, container=None, results=None
 
     ################################################################################
     ## Custom Code End
-    ################################################################################    
+    ################################################################################
 
-    # call custom function "community/datetime_modify", returns the custom_function_run_id
-    phantom.custom_function(custom_function='community/datetime_modify', parameters=parameters, name='calculate_start_time', callback=join_filter_unused_and_not_none)
+    phantom.act("list users", parameters=parameters, name="list_all_accounts", assets=["aws_iam"], callback=join_filter_inactive_accounts)
 
     return
 
-"""
-Compare the PasswordLastUsed field to the calculated start time to find unused accounts. Ignore accounts with no value for PasswordLastUsed. This will ignore all accounts with no passwords, such as accounts that only use API access keys.
-"""
-def filter_unused_and_not_none(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('filter_unused_and_not_none() called')
 
-    # collect filtered artifact ids for 'if' condition 1
+def calculate_start_time(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("calculate_start_time() called")
+
+    parameters = []
+
+    parameters.append({
+        "input_datetime": None,
+        "input_format_string": None,
+        "modification_unit": "days",
+        "amount_to_modify": -90,
+        "output_format_string": "%Y-%m-%dT%H:%M:%SZ",
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/datetime_modify", parameters=parameters, name="calculate_start_time", callback=join_filter_inactive_accounts)
+
+    return
+
+
+def join_filter_inactive_accounts(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("join_filter_inactive_accounts() called")
+
+    if phantom.completed(action_names=["list_all_accounts"], custom_function_names=["calculate_start_time"]):
+        # call connected block "filter_inactive_accounts"
+        filter_inactive_accounts(container=container, handle=handle)
+
+    return
+
+
+def filter_inactive_accounts(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("filter_inactive_accounts() called")
+
+    ################################################################################
+    # Compare the PasswordLastUsed field to the calculated start time to find unused 
+    # accounts. Ignore accounts with no value for PasswordLastUsed. This will ignore 
+    # all accounts with no passwords, such as accounts that only use API access keys.
+    ################################################################################
+
+    # collect filtered artifact ids and results for 'if' condition 1
     matched_artifacts_1, matched_results_1 = phantom.condition(
         container=container,
-        action_results=results,
+        logical_operator="and",
         conditions=[
             ["list_all_accounts:action_result.data.*.PasswordLastUsed", "<", "calculate_start_time:custom_function_result.data.datetime_string"],
-            ["list_all_accounts:action_result.data.*.PasswordLastUsed", "!=", ""],
+            ["list_all_accounts:action_result.data.*.PasswordLastUsed", "!=", ""]
         ],
-        logical_operator='and',
-        name="filter_unused_and_not_none:condition_1")
+        name="filter_inactive_accounts:condition_1")
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        get_unused_account_info(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        get_unused_account_information(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     return
 
-def join_filter_unused_and_not_none(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None):
-    phantom.debug('join_filter_unused_and_not_none() called')
 
-    # check if all connected incoming playbooks, actions, or custom functions are done i.e. have succeeded or failed
-    if phantom.completed(action_names=['list_all_accounts'], custom_function_names=['calculate_start_time']):
-        
-        # call connected block "filter_unused_and_not_none"
-        filter_unused_and_not_none(container=container, handle=handle)
-    
+def get_unused_account_information(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("get_unused_account_information() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    ################################################################################
+    # Use the "get user" action to gather more information about the unused accounts, 
+    # including group membership and policy assignments.
+    ################################################################################
+
+    filtered_result_0_data_filter_inactive_accounts = phantom.collect2(container=container, datapath=["filtered-data:filter_inactive_accounts:condition_1:list_all_accounts:action_result.data.*.UserName"])
+
+    parameters = []
+
+    # build parameters list for 'get_unused_account_information' call
+    for filtered_result_0_item_filter_inactive_accounts in filtered_result_0_data_filter_inactive_accounts:
+        if filtered_result_0_item_filter_inactive_accounts[0] is not None:
+            parameters.append({
+                "username": filtered_result_0_item_filter_inactive_accounts[0],
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("get user", parameters=parameters, name="get_unused_account_information", assets=["aws_iam"], callback=save_to_artifacts)
+
     return
 
-"""
-Create an artifact in the parent event for each unused account that was found. The CEF type will allow future automation to detect that these are AWS accounts.
-"""
+
 def save_to_artifacts(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('save_to_artifacts() called')
-        
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'save_to_artifacts' call
-    results_data_1 = phantom.collect2(container=container, datapath=['get_unused_account_info:action_result.parameter.username', 'get_unused_account_info:action_result.parameter.context.artifact_id'], action_results=results)
+    phantom.debug("save_to_artifacts() called")
+
+    id_value = container.get("id", None)
+    get_unused_account_information_result_data = phantom.collect2(container=container, datapath=["get_unused_account_information:action_result.parameter.username","get_unused_account_information:action_result.parameter.context.artifact_id"], action_results=results)
 
     parameters = []
-    
+
     # build parameters list for 'save_to_artifacts' call
-    for results_item_1 in results_data_1:
-        if results_item_1[0]:
-            parameters.append({
-                'name': "Unused AWS Account",
-                'label': "user",
-                'cef_name': "awsUserName",
-                'contains': "aws iam user name",
-                'cef_value': results_item_1[0],
-                'container_id': "",
-                'cef_dictionary': "",
-                'run_automation': False,
-                'source_data_identifier': results_item_1[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': results_item_1[1]},
-            })
+    for get_unused_account_information_result_item in get_unused_account_information_result_data:
+        parameters.append({
+            "container": id_value,
+            "name": "Unused AWS Account",
+            "label": "user",
+            "severity": None,
+            "cef_field": "awsUserName",
+            "cef_value": get_unused_account_information_result_item[0],
+            "cef_data_type": "aws iam user name",
+            "tags": None,
+            "run_automation": "false",
+            "input_json": None,
+        })
 
-    phantom.act(action="add artifact", parameters=parameters, assets=['phantom'], name="save_to_artifacts", parent_action=action)
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
 
-    return
+    # Write your custom code here...
 
-"""
-List all AWS IAM accounts, which will include the PasswordLastUsed field for us to filter on.
-"""
-def list_all_accounts(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('list_all_accounts() called')
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
 
-    # collect data for 'list_all_accounts' call
-
-    parameters = []
-    
-    # build parameters list for 'list_all_accounts' call
-    parameters.append({
-        'user_path': "/",
-        'group_name': "",
-    })
-
-    phantom.act(action="list users", parameters=parameters, assets=['aws_iam'], callback=join_filter_unused_and_not_none, name="list_all_accounts")
+    phantom.custom_function(custom_function="community/artifact_create", parameters=parameters, name="save_to_artifacts", callback=playbook_aws_disable_user_accounts_1)
 
     return
 
-"""
-Use the "get user" action to gather more information about the unused accounts, including group membership and policy assignments.
-"""
-def get_unused_account_info(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('get_unused_account_info() called')
-        
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'get_unused_account_info' call
-    filtered_results_data_1 = phantom.collect2(container=container, datapath=["filtered-data:filter_unused_and_not_none:condition_1:list_all_accounts:action_result.data.*.UserName", "filtered-data:filter_unused_and_not_none:condition_1:list_all_accounts:action_result.parameter.context.artifact_id"])
 
-    parameters = []
-    
-    # build parameters list for 'get_unused_account_info' call
-    for filtered_results_item_1 in filtered_results_data_1:
-        if filtered_results_item_1[0]:
-            parameters.append({
-                'username': filtered_results_item_1[0],
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': filtered_results_item_1[1]},
-            })
+def playbook_aws_disable_user_accounts_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("playbook_aws_disable_user_accounts_1() called")
 
-    phantom.act(action="get user", parameters=parameters, assets=['aws_iam'], callback=save_to_artifacts, name="get_unused_account_info")
+    get_unused_account_information_result_data = phantom.collect2(container=container, datapath=["get_unused_account_information:action_result.parameter.username"], action_results=results)
+
+    get_unused_account_information_parameter_username = [item[0] for item in get_unused_account_information_result_data]
+
+    inputs = {
+        "aws_username": get_unused_account_information_parameter_username,
+    }
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    # call playbook "community/aws_disable_user_accounts", returns the playbook_run_id
+    playbook_run_id = phantom.playbook("community/aws_disable_user_accounts", container=container, inputs=inputs)
 
     return
+
 
 def on_finish(container, summary):
-    phantom.debug('on_finish() called')
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
     # This function is called after all actions are completed.
     # summary of all the action and/or all details of actions
     # can be collected here.
@@ -181,5 +227,9 @@ def on_finish(container, summary):
             # if 'action_run_id' in action_result:
                 # action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
                 # phantom.debug(action_results)
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
 
     return
