eda32e55af000157353e1bb5f04379373927cc05
Philip Royer <proyer@splunk.com>
2021-11-01 16:31:07 -0400

    update aws_disable_user_accounts for new vpe and inputs

--- aws_disable_user_accounts.py
+++ aws_disable_user_accounts.py
@@ -1,127 +1,170 @@
 """
-Disable a list of AWS IAM user accounts. After checking the list of accounts against an allowlist and confirming with an analyst, each account is disabled. The effect can be reversed with the "enable user" action.
+Disable a list of AWS IAM user accounts. After checking the list of accounts against an allowlist and confirming with an analyst, each account is disabled. The change can be reversed with the &quot;enable user&quot; action.
 """
+
 
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
+
+
 def on_start(container):
     phantom.debug('on_start() called')
-    
+
     # call 'filter_1' block
     filter_1(container=container)
 
     return
 
-"""
-Prompt the analyst with a list of accounts to confirm the accounts should be disabled.
-"""
-def aws_reset_inactive_user(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('aws_reset_inactive_user() called')
-    
-    # set user and message variables for phantom.prompt call
-    user = "admin"
-    message = """The following AWS user(s) were found to be inactive:
-
-```
-{0}
-```
-
-Should those user account(s) be disabled?"""
-
-    # parameter list for template variable replacement
-    parameters = [
-        "filtered-data:filter_1:condition_1:artifact:*.cef.awsUserName",
-    ]
-
-    #responses:
-    response_types = [
-        {
-            "prompt": "",
-            "options": {
-                "type": "list",
-                "choices": [
-                    "Yes",
-                    "No",
-                ]
-            },
-        },
-    ]
-
-    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="aws_reset_inactive_user", parameters=parameters, response_types=response_types, callback=decision_1)
-
-    return
-
-"""
-Check the allowlist to determine if any user account should be skipped by this playbook.
-"""
 def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('filter_1() called')
+    phantom.debug("filter_1() called")
 
-    # collect filtered artifact ids for 'if' condition 1
+    # collect filtered artifact ids and results for 'if' condition 1
     matched_artifacts_1, matched_results_1 = phantom.condition(
         container=container,
         conditions=[
-            ["artifact:*.cef.awsUserName", "not in", "custom_list:aws_inactive_user_allowlist"],
+            ["playbook_input:aws_username", "in", "custom_list:aws_inactive_user_allowlist"]
         ],
         name="filter_1:condition_1")
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        aws_reset_inactive_user(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        indicator_tag_4(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    # collect filtered artifact ids and results for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        conditions=[
+            ["playbook_input:aws_username", "not in", "custom_list:aws_inactive_user_allowlist"]
+        ],
+        name="filter_1:condition_2")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        aws_disable_user_check(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
 
     return
 
-"""
-Disable the accounts that were not in the allowlist.
-"""
-def disable_user_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('disable_user_1() called')
-        
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'disable_user_1' call
-    filtered_artifacts_data_1 = phantom.collect2(container=container, datapath=['filtered-data:filter_1:condition_1:artifact:*.cef.awsUserName', 'filtered-data:filter_1:condition_1:artifact:*.id'])
 
-    parameters = []
-    
-    # build parameters list for 'disable_user_1' call
-    for filtered_artifacts_item_1 in filtered_artifacts_data_1:
-        if filtered_artifacts_item_1[0]:
-            parameters.append({
-                'username': filtered_artifacts_item_1[0],
-                'disable_access_keys': "true",
-                # context (artifact id) is added to associate results with the artifact
-                'context': {'artifact_id': filtered_artifacts_item_1[1]},
-            })
+def aws_disable_user_check(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("aws_disable_user_check() called")
 
-    phantom.act(action="disable user", parameters=parameters, assets=['aws_iam'], name="disable_user_1")
+    # set user and message variables for phantom.prompt call
+
+    user = "proyer"
+    message = """The following AWS user(s) were found to be inactive:\n\n```\n{0}\n```"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "filtered-data:filter_1:condition_2:playbook_input:aws_username"
+    ]
+
+    # responses
+    response_types = [
+        {
+            "prompt": "Should those user account(s) be disabled?",
+            "options": {
+                "type": "list",
+                "choices": [
+                    "Yes",
+                    "No"
+                ],
+            },
+        }
+    ]
+
+    phantom.prompt2(container=container, user=user, message=message, respond_in_mins=30, name="aws_disable_user_check", parameters=parameters, response_types=response_types, callback=decision_1)
 
     return
 
-"""
-Only proceed if the analyst responds "Yes".
-"""
+
 def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('decision_1() called')
+    phantom.debug("decision_1() called")
 
     # check for 'if' condition 1
-    matched = phantom.decision(
+    found_match_1 = phantom.decision(
         container=container,
-        action_results=results,
         conditions=[
-            ["aws_reset_inactive_user:action_result.summary.responses.0", "==", "Yes"],
+            ["aws_disable_user_check:action_result.summary.responses.0", "==", "Yes"]
         ])
 
     # call connected blocks if condition 1 matched
-    if matched:
-        disable_user_1(action=action, success=success, container=container, results=results, handle=handle, custom_function=custom_function)
+    if found_match_1:
+        disable_user_1(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     return
 
+
+def disable_user_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("disable_user_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    filtered_input_0_aws_username = phantom.collect2(container=container, datapath=["filtered-data:filter_1:condition_2:playbook_input:aws_username"])
+
+    parameters = []
+
+    # build parameters list for 'disable_user_1' call
+    for filtered_input_0_aws_username_item in filtered_input_0_aws_username:
+        if filtered_input_0_aws_username_item[0] is not None:
+            parameters.append({
+                "username": filtered_input_0_aws_username_item[0],
+                "disable_access_keys": True,
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("disable user", parameters=parameters, name="disable_user_1", assets=["aws_iam"])
+
+    return
+
+
+def indicator_tag_4(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("indicator_tag_4() called")
+
+    filtered_input_0_aws_username = phantom.collect2(container=container, datapath=["filtered-data:filter_1:condition_1:playbook_input:aws_username"])
+
+    parameters = []
+
+    # build parameters list for 'indicator_tag_4' call
+    for filtered_input_0_aws_username_item in filtered_input_0_aws_username:
+        parameters.append({
+            "indicator": filtered_input_0_aws_username_item[0],
+            "tags": "aws_inactive_user_allowlist",
+            "overwrite": None,
+        })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/indicator_tag", parameters=parameters, name="indicator_tag_4")
+
+    return
+
+
 def on_finish(container, summary):
-    phantom.debug('on_finish() called')
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
     # This function is called after all actions are completed.
     # summary of all the action and/or all details of actions
     # can be collected here.
@@ -132,5 +175,9 @@ def on_finish(container, summary):
             # if 'action_run_id' in action_result:
                 # action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
                 # phantom.debug(action_results)
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
 
     return
