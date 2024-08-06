10cc5dbb9939436f50a9b7483e047499c6eccfd2
Daniel Federschmidt <dfederschmidt@splunk.com>
2024-08-06 16:41:18 +0200

    feat: first take at a release pipeline

---
+++ .github/tests/robot/PlaybookScannerHelper.py
@@ -0,0 +1,30 @@
+from splunk_soar_sdk import ParsedPlaybook
+
+from robot.libraries.BuiltIn import BuiltIn
+
+
+class PlaybookScannerHelper:
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+
+    def helper_parse_playbook(self, name_prefix: str) -> ParsedPlaybook:
+        """Prepares a playbook for testing.
+
+        Args:
+            name_prefix: Path to playbook files, without extension.
+
+        Returns:
+            Parsed Playbook object.
+        """
+        pb = ParsedPlaybook.from_text(name_prefix)
+        return pb
+
+    def copy_playbook(self, pb: ParsedPlaybook) -> ParsedPlaybook:
+        """Copies a playbook.
+
+        Args:
+            pb: Parsed playbook object to be copied.
+
+        Returns:
+            Copied parsed Playbook object.
+        """
+        return ParsedPlaybook.from_b64(pb.to_b64())
