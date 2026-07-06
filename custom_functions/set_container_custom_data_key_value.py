392655d08626f1ae4e6aaab0539eb09fe32ef6d1
Deep Basu <38263849+bdeep4splunk@users.noreply.github.com>
2026-07-06 22:26:40 +0530

    Add get_container_custom_data_key and set_container_custom_data_key_value custom functions (#233)
    * Add helper Custom Functions used in Phishing OSS playbooks to set and get key value pair from a container's custom_fields
    
    * Fixed naming convention, potential KeyError crash in set_container_custom_data_key_value and added descriptions to fields and outputs.
    
    * Fixed filenames
    
    * Added the get functions back
    
    * Fixed filename typos for set functions
    
    * Fixed custom function name for set_container_custom_data_key_value

---
+++ custom_functions/set_container_custom_data_key_value.py
@@ -0,0 +1,38 @@
+def set_container_custom_data_key_value(container_id=None, custom_key=None, custom_value=None, **kwargs):
+    """
+    This custom function sets/updates a key-value pair in a container's custom_fields.
+    
+    Args:
+        container_id: Container ID
+        custom_key: The name of the custom field to set
+        custom_value: The value to store
+    
+    Returns a JSON-serializable object that implements the configured data paths:
+        custom_key: Returns
+            {
+              "<custom_key>": "<custom_value>",
+              "Container_Id": "<container_id>"
+            }
+    """
+    ############################ Custom Code Goes Below This Line #################################
+    import json
+    import phantom.rules as phantom
+    
+    outputs = {}
+    
+    # Write your custom code here...
+    phantom.debug(custom_value)
+    phantom.debug(custom_key)
+    container=phantom.get_container(container_id)
+    #phantom.debug(container)
+    container_data = container.get('custom_fields',{})
+    container_data[custom_key] = custom_value
+    
+    phantom.debug(container_data)
+    success, message = phantom.update(container, { "custom_fields": container_data})
+    
+    outputs={custom_key:custom_value,"Container_Id":container_id}
+    
+    # Return a JSON-serializable object
+    assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
+    return outputs
